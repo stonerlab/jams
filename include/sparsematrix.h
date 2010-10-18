fabb6963333ca2b2a7a5b40a7090a1984eb441b5
Joe Barker <jb544@york.ac.uk>
2010-10-18 16:25:42 +0100

    Moved templated members into header.

--- include/sparsematrix.h
+++ include/sparsematrix.h
@@ -2,6 +2,8 @@
 #define __SPARSEMATRIX_H__
 
 #include <vector>
+#include <cassert>
+#include "globals.h"
 
 #define RESTRICT __restrict__
 
@@ -30,14 +32,17 @@ class SparseMatrix {
         nrows(m),
         ncols(n),
         nnz(0),
-        val(nnz_guess),
-        row(nnz_guess),
-        col(nnz_guess)
-    {}
+        val(0),
+        row(0),
+        col(0)
+    {val.reserve(nnz_guess); row.reserve(nnz_guess); col.reserve(nnz_guess);}
 
-    void insert(size_type i, size_type j, _Tp &value);
+    void insert(size_type i, size_type j, _Tp value);
 
     double memorySize();
+
+    void printCSR();
+
     void coocsr();
 
     inline int nonzero() { return nnz; }
@@ -74,5 +79,98 @@ class SparseMatrix {
     std::vector<size_type> col;
 
 };
+
+template <typename _Tp>
+void SparseMatrix<_Tp>::printCSR() {
+
+  if(format==CSR) {
+    for(int i=0; i<nrows+1; ++i) {
+      output.write("%i\n",row[i]);
+    }
+    output.write("\n\n");
+    for(int i=0; i<nnz; ++i) {
+      output.write("%i\n",col[i]);
+    }
+  }
+}
+
+template <typename _Tp>
+double SparseMatrix<_Tp>::memorySize() {
+  const double mb = (1024.0*1024.0);
+  return (((row.size()+col.size())*sizeof(size_type))+(val.size()*sizeof(_Tp)))/mb;
+}
+
+template <typename _Tp>
+void SparseMatrix<_Tp>::insert(size_type i, size_type j, _Tp value) {
+  if(format != COO) {
+    jams_error("Can only insert into COO format sparse matrix");
+  }
+
+  assert(i < nrows);
+  assert(j < ncols);
+
+  row.push_back(i);
+  col.push_back(j);
+  val.push_back(value);
+
+  nnz++;
+}
+
+//coo-csr conversion from fortran sparsekit
+// http://people.sc.fsu.edu/~jburkardt/f77_src/sparsekit/sparsekit.f
+template <typename _Tp>
+void SparseMatrix<_Tp>::coocsr()
+{
+  if(format == CSR) {
+    output.write("WARNING: Cannot convert SparseMatrix");
+    output.write("Matrix is alread in CSR format");
+    return;
+  }
+
+  std::vector<size_type>  csrrow((nrows+1),0);
+  std::vector<size_type>  csrcol(nnz,0);
+  std::vector<_Tp>     csrval(nnz);
+
+  // determine row lengths
+  for(int k=0; k<nnz; ++k) {
+    csrrow[row[k]]++;
+  }
+
+
+  // starting poition of each row
+  int p=0;
+  for(int j=0; j<(nrows+1); ++j) {
+    int p0 = csrrow[j];
+    csrrow[j] = p;
+    p = p+p0;
+  }
+
+  // go through the structure once more, fill in the output matrix
+
+  for(int k=0; k<nnz; ++k) {
+    const int i = row[k];
+    const int j = col[k];
+    const _Tp x = val[k];
+    const _Tp ia = csrrow[i];
+    csrval[ia] = x;
+    csrcol[ia] = j;
+    csrrow[i] = ia+1;
+  }
+
+  // shift back csrrow
+  for(int j=0; j<nrows+1; ++j) {
+    const int idx = nrows+1-j;
+    csrrow[idx] = csrrow[idx-1];
+  }
+  csrrow[0] = 0;
+
+  row.resize(nrows+1);
+  row.swap(csrrow);
+  val.swap(csrval);
+  col.swap(csrcol);
+
+  format = CSR;
+}
+
 
 #endif // __SPARSEMATRIX_H__
