0b1aefd7cbdfc1735e63ce595898edf31945de77
Joe Barker <jb544@york.ac.uk>
2010-10-18 09:02:43 +0100

    Removed vec3 struct, no longer needed.

--- include/lattice.h
+++ include/lattice.h
@@ -4,21 +4,6 @@
 #include <map>
 #include <vector>
 
-// struct for convenience
-template <typename _Tp>
-struct vec3
-{
-  public:
-    vec3(){};
-    vec3(const _Tp x, const _Tp y, const _Tp z)
-      {data[0]=x;data[1]=y;data[2]=z;}
-    _Tp RESTRICT &operator[](const int i){return data[i];}
-    const _Tp &operator[](const int i) const {return data[i];}
-  private:
-    _Tp data[3];
-};
-
-
 class Lattice {
   public:
     Lattice() : dim(3,0), ntypes(0), atom_type(0,0), atom_type_map() {}
