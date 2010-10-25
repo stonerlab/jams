62e747b3d2f1298ae339c22722062cb069474a34
Joe Barker <jb544@york.ac.uk>
2010-10-25 14:08:59 +0100

    Initialise class with member list.

--- include/rand.h
+++ include/rand.h
@@ -4,10 +4,27 @@ class Random;
 #define __RAND_H__
 
 #include <stdint.h>
+#include <vector>
 
 class Random 
 {
   public:
+    Random() : 
+      initialised(false),
+      init_seed(0),
+      ul_limit(std::numeric_limits<uint32_t>::max()),
+      norm_open(1.0/static_cast<double>(ul_limit)),
+      norm_closed(1.0/static_cast<double>(ul_limit-1)),
+      mwc_x(0),
+      cmwc_q(4096,0),
+      cmwc_c(0),
+      cmwc_r(0),
+      discrete_ival(0),
+      discrete_rlim(0),
+      normal_logic(false),
+      normal_next(0.0)
+    {}
+
     void seed(const unsigned long &x);
     double uniform();
     double uniform_open();
@@ -18,9 +35,13 @@ class Random
     bool initialised;
     unsigned long init_seed;
 
+    const uint32_t ul_limit;
+    const double norm_open;
+    const double norm_closed;
+
     uint64_t mwc_x;
 
-    uint32_t cmwc_q[4096];
+    std::vector<uint32_t> cmwc_q;
     uint32_t cmwc_c;
     uint32_t cmwc_r;
 
@@ -35,17 +56,17 @@ class Random
     }
 
     uint32_t cmwc4096() {
-      const uint64_t a = static_cast<uint64_t>(18782);
-      const uint32_t m = static_cast<uint32_t>(0xfffffffe);
+//      const uint64_t a = static_cast<uint64_t>(18782);
+//      const uint32_t m = static_cast<uint32_t>(0xfffffffe);
       uint64_t t;
       uint32_t x;
 
       cmwc_r = ( cmwc_r + 1 ) & 4095;
-      t = a * cmwc_q[cmwc_r] + cmwc_c;
+      t = static_cast<uint64_t>(18782) * cmwc_q[cmwc_r] + cmwc_c;
       cmwc_c = (t >> 32);
       x = t + cmwc_c;
       if( x < cmwc_c ){ cmwc_r++; cmwc_c++; }
-      return ( cmwc_q[cmwc_r] = (m - x) );
+      return ( cmwc_q[cmwc_r] = (static_cast<uint32_t>(0xfffffffe) - x) );
     }
     
 };
