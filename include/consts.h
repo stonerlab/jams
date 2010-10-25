c878fea38411690538af513097b59f8c8011be87
Joe Barker <jb544@york.ac.uk>
2010-10-25 14:08:36 +0100

    Added some physics constants.

--- include/consts.h
+++ include/consts.h
@@ -8,5 +8,8 @@
 #endif
 
 const double pi = 3.14159265358979323846;
+const double mu_bohr_si = 9.27400915E-24;
+const double gamma_electron_si = 1.760859770e11;
+const double boltzmann_si = 1.3806504e-23;
 
 #endif // __CONSTS_H__
