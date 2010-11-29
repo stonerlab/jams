4ad824151d9bc0ebdbcb68a6df4f3e93e20a4463
Joe Barker <jb544@york.ac.uk>
2010-11-29 11:13:23 +0000

    Use mu_s normalisation (close to 1 for float on GPU) and implement MKL sparse blas.

--- src/fields.cc
+++ src/fields.cc
@@ -2,19 +2,30 @@
 #include "globals.h"
 #include "sparsematrix.h"
 
+#ifdef MKL
+#include <mkl_spblas.h>
+#endif
+
 void calculate_fields()
 {
   using namespace globals;
  
   // dscrmv below has beta=0.0 -> field array is zeroed
   // exchange
-  const char transa[1] = {'N'};
-  const char matdescra[6] = {'S','L','N','C','N','N'};
+  char transa[1] = {'N'};
+  char matdescra[6] = {'S','L','N','C','N','N'};
+  double one=1.0;
+  double zero=0.0;
   int i,j;
 
   if(Jij.nonzero() > 0) {
+#ifdef MKL
+    mkl_dcsrmv(transa,&nspins3,&nspins3,&one,matdescra,Jij.ptrVal(),
+        Jij.ptrCol(), Jij.ptrB(),Jij.ptrE(),s.ptr(),&zero,h.ptr());
+#else
     jams_dcsrmv(transa,nspins3,nspins3,1.0,matdescra,Jij.ptrVal(),
-        Jij.ptrCol(), Jij.ptrB(),Jij.ptrE(),s.ptr(),0.0,h.ptr()); 
+        Jij.ptrCol(), Jij.ptrB(),Jij.ptrE(),s.ptr(),0.0,h.ptr());
+#endif
   } else {
     std::fill(h.ptr(),h.ptr()+nspins3,0.0); 
   }
@@ -22,7 +33,7 @@ void calculate_fields()
   // normalize by the gyroscopic factor
   for(i=0; i<nspins; ++i) {
     for(j=0; j<3;++j) {
-      h(i,j) = (h(i,j)+ (w(i,j)+h_app[j])*mus(i) )*gyro(i);
+      h(i,j) = ( h(i,j) + w(i,j) + h_app[j])*gyro(i);
     }
   }
 }
