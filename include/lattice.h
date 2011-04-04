4e15a4cf651bd6080a2e22463dcb04a5d16cf006
Joe Barker <jb544@york.ac.uk>
2011-04-04 10:14:43 +0100

    Added getDimensions method to lattice class to allow other parts of the code to get lattice information.

--- include/lattice.h
+++ include/lattice.h
@@ -10,6 +10,7 @@ class Lattice {
     Lattice() : dim(3,0), nTypes(0), atom_type(0,0), type_count(0), atomTypeMap() {}
     void createFromConfig(libconfig::Config &config);
 
+    inline void getDimensions(int &x, int &y, int& z) { x = dim[0]; y = dim[1]; z = dim[2]; }
     inline int getType(const int i) { return atom_type[i]; }
     inline int getTypeCount(const int i) { return type_count[i]; }
     inline int numTypes() { return nTypes; }
