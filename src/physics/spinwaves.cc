2951946cf8f1fa42aa35b0acad570507590542d1
Joe Barker <jb544@york.ac.uk>
2011-06-29 09:36:45 +0100

    Simplify an clarify spinwave code for structure factor calculation.

--- src/physics/spinwaves.cc
+++ src/physics/spinwaves.cc
@@ -93,21 +93,21 @@ void SpinwavesPhysics::init(libconfig::Setting &phys)
     }else{
       s(i,0) = 0.0;
       s(i,1) = 0.0;
-      s(i,2) = -1.0;
+      s(i,2) = 1.0;
     }
   }
 
   // read from config if spinstates should be dumped
   // (default is false)
-  spinDump = false;
-  phys.lookupValue("spindump",spinDump);
+  //spinDump = false;
+  //phys.lookupValue("spindump",spinDump);
 
   // open spin dump file as binary file
   filename = "_spd.dat";
   filename = seedname+filename;
-  if(spinDump == true){
-    SPDFile.open(filename.c_str(),std::ofstream::binary);
-  }
+  //if(spinDump == true){
+  //  SPDFile.open(filename.c_str(),std::ofstream::binary);
+  //}
 
   const double sampletime = config.lookup("sim.t_out");
   const double runtime = config.lookup("sim.t_run");
@@ -145,12 +145,7 @@ void SpinwavesPhysics::run(double realtime, const double dt) {
 void SpinwavesPhysics::monitor(double realtime, const double dt) {
   using namespace globals;
   
-//  for(int i=0; i<nspins; ++i){
-//    FFTArray[i][0] = s(i,0);
-//    FFTArray[i][1] = s(i,1);
-//  }
-
-
+  // calculate structure factor <S^{k}(-q)S^{k}(q)>
   double magA[3] = {0.0,0.0,0.0};
   double magB[3] = {0.0,0.0,0.0};
   
@@ -174,112 +169,56 @@ void SpinwavesPhysics::monitor(double realtime, const double dt) {
     magB[j] = magB[j]/(0.5*nspins);
   }
 
-
-  double phi = acos(magA[2]*normA);
-  double theta   = atan2(magA[0],magA[1]);
-
-  // calculate rotation matrix for rotating m->m_z
-  double c_t = cos(theta);
-  double c_p = cos(phi);
-  double s_t = sin(theta);
-  double s_p = sin(phi);
-
-  double rotA[3][3];
-
-  // R_z(phi)*R_y(theta)
-//   rot[0][0]=c_t*c_p;
-//   rot[0][1]=-s_p;
-//   rot[0][2]=c_p*s_t;
-//   rot[1][0]=s_p*c_t;
-//   rot[1][1]=c_p;
-//   rot[1][2]=s_t*s_p;
-//   rot[2][0]=-s_t;
-//   rot[2][1]=0.0;
-//   rot[2][2]=c_t;
-
-  rotA[0][0]=c_t*c_p;
-  rotA[0][1]=s_t*c_p;
-  rotA[0][2]=-s_p;
-  rotA[1][0]=-s_t;
-  rotA[1][1]=c_t;
-  rotA[1][2]=0.0;
-  rotA[2][0]=c_t*s_p;
-  rotA[2][1]=s_t*s_p;
-  rotA[2][2]=c_p;
-
-  phi = acos(magB[2]*normB);
-  theta   = atan2(magB[0],magB[1]);
-
-  // calculate rotation matrix for rotating m->m_z
-  c_t = cos(theta);
-  c_p = cos(phi);
-  s_t = sin(theta);
-  s_p = sin(phi);
-
-  double rotB[3][3];
-//   rotB[0][0]=c_t*c_p;
-//   rotB[0][1]=s_t*c_p;
-//   rotB[0][2]=-s_p;
-//   rotB[1][0]=-s_t;
-//   rotB[1][1]=c_t;
-//   rotB[1][2]=0.0;
-//   rotB[2][0]=c_t*s_p;
-//   rotB[2][1]=s_t*s_p;
-//   rotB[2][2]=c_p;
-
-//   rotB[0][0]=c_t*c_p;
-//   rotB[0][1]=-s_t;
-//   rotB[0][2]=c_t*s_p;
-//   rotB[1][0]=s_t*c_p;
-//   rotB[1][1]=c_t;
-//   rotB[1][2]=s_t*s_p;
-//   rotB[2][0]=-s_p;
-//   rotB[2][1]=0.0;
-//   rotB[2][2]=c_p;
-
   for(int i=0; i<nspins; ++i){
 
     if(typeOverride[i] == 0){
-      FFTArray[i][0] = rotA[0][0]*s(i,0) + rotA[0][1]*s(i,1) + rotA[0][2]*s(i,2);
-      FFTArray[i][1] = rotA[1][0]*s(i,0) + rotA[1][1]*s(i,1) + rotA[1][2]*s(i,2);
+      FFTArray[i][0] = s(i,0);
+      FFTArray[i][1] = s(i,1);
     } else {
-      FFTArray[i][0] = (rotA[0][0]*(-s(i,0)) + rotA[0][1]*s(i,1) + rotA[0][2]*(-s(i,2)));
-      FFTArray[i][1] = (rotA[1][0]*(-s(i,0)) + rotA[1][1]*s(i,1) + rotA[1][2]*(-s(i,2)));
+      FFTArray[i][0] = -s(i,0);
+//       FFTArray[i][0] = s(i,0);
+      FFTArray[i][1] = s(i,1);
     }
   }
 
-  if(spinDump == true){
-    // write spin dump data in binary
-    SPDFile.write((char*)FFTArray,nspins*sizeof(fftw_complex));
-  }
+//   if(spinDump == true){
+//     // write spin dump data in binary
+//     SPDFile.write((char*)FFTArray,nspins*sizeof(fftw_complex));
+//   }
 
   fftw_execute(FFTPlan);
 
+  FFTArray[0][0] = 2.0*FFTArray[0][0];
+  FFTArray[0][1] = 2.0*FFTArray[0][1];
+
+  
+
   // normalise by number of spins and total mode power
   double pow_norm = 0.0;
-  for(int i=0; i<(dim[0]/2)+1; ++i){
-    for(int j=0; j<(dim[1]/2)+1; ++j){
-      for(int k=0; k<(dim[2]/2)+1; ++k){
+  for(int i=0; i<dim[0]; ++i){
+    for(int j=0; j<dim[1]; ++j){
+      for(int k=0; k<dim[2]; ++k){
         const int kVec[3] = {i,j,k};
         const int idx = kVec[2] + dim[2]*(kVec[1] + dim[1]*kVec[0]);
-        pow_norm += (FFTArray[idx][0]*FFTArray[idx][0] + FFTArray[idx][1]*FFTArray[idx][1]);
+        FFTArray[idx][0] = FFTArray[idx][0]/sqrt(dim[0]*dim[1]*dim[2]);
+        FFTArray[idx][1] = FFTArray[idx][1]/sqrt(dim[0]*dim[1]*dim[2]);
       }
     }
   }
   
-  const double kzero = (FFTArray[0][0]*FFTArray[0][0] + FFTArray[0][1]*FFTArray[0][1])/(pow_norm);
+  const double kzero = (FFTArray[0][0]*FFTArray[0][0] + FFTArray[0][1]*FFTArray[0][1]);
 
-  SPWFile << realtime << "\t" << kzero << "\t" << 1.0-kzero;
-  SPWFile << "\t"<< magA[0] << "\t" << magA[1] << "\t" << magA[2];
-  SPWFile << "\t"<< magB[0] << "\t" << magB[1] << "\t" << magB[2];
+  SPWFile << 0 << "\t" << kzero << "\n";
 
    for(int k=1; k<(dim[2]/2)+1; ++k){
-     const int kVec[3] = {0,0,k};
-     const int idx = kVec[2] + dim[2]*(kVec[1] + dim[1]*kVec[0]);
-     double pow = (FFTArray[idx][0]*FFTArray[idx][0] + FFTArray[idx][1]*FFTArray[idx][1]);
-     SPWFile << "\t" << pow/pow_norm;
+     const int qVec[3]      = {0,0,k};
+     const int qVecMinus[3] = {0,0,(dim[2]-k)};
+     const int qIdx      = qVec[2] + dim[2]*(qVec[1] + dim[1]*qVec[0]);
+     const int qIdxMinus = qVecMinus[2] + dim[2]*(qVecMinus[1] + dim[1]*qVecMinus[0]);
+     double Sq      = (FFTArray[qIdx][0]*FFTArray[qIdx][0] + FFTArray[qIdx][1]*FFTArray[qIdx][1]);
+     double SqMinus = (FFTArray[qIdxMinus][0]*FFTArray[qIdxMinus][0] + FFTArray[qIdxMinus][1]*FFTArray[qIdxMinus][1]);
+     SPWFile << k << "\t" << (Sq*SqMinus) << "\n";
    }
-  SPWFile << "\n";
+  SPWFile << "\n\n";
 
-  fftw_execute(FFTPlan);
 }
