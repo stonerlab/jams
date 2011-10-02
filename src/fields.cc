1c94c9a1778c6026bf09d66455c359cb81c3c3cb
Joe Barker <jb544@york.ac.uk>
2011-10-02 22:04:37 +0100

    Added biquadratic field routine.

--- src/fields.cc
+++ src/fields.cc
@@ -6,6 +6,42 @@
 #include <mkl_spblas.h>
 #endif
 
+void calculate_biquadratic(const double *val, const int *indx, 
+  const int *ptrb, const int *ptre)
+{
+  using namespace globals;
+
+  double tmp;
+  int i,j,k,l;
+  int begin,end;
+
+  if( J2ij.nonZero() > 0 ){
+
+    for(i=0; i<nspins; ++i) { // iterate rows
+      begin = ptrb[i]; end = ptre[i];
+      for(j=begin; j<end; ++j) {
+        k = indx[j];  // column
+        // upper triangle and diagonal
+        tmp = (s(i,0)*s(k,0) + s(i,1)*s(k,1) + s(i,2)*s(k,2))*val[j];
+        if ( i > (k-1) ){
+          for(l=0; l<3; ++l){
+            h(i,l) = h(i,l) + s(k,l)*tmp;
+          }
+        }
+      }
+      for(j=begin; j<end; ++j) {
+        k = indx[j];  // column
+        // upper triangle and diagonal
+        tmp = (s(i,0)*s(k,0) + s(i,1)*s(k,1) + s(i,2)*s(k,2))*val[j];
+        if ( i > k ){
+          for(l=0; l<3; ++l){
+            h(k,l) = h(k,l) + s(i,l)*tmp;
+          }
+        }
+      }
+    }
+  }
+}
 void calculate_fields()
 {
   using namespace globals;
@@ -29,6 +65,8 @@ void calculate_fields()
   } else {
     std::fill(h.ptr(),h.ptr()+nspins3,0.0); 
   }
+
+  calculate_biquadratic(J2ij.valPtr(),J2ij.colPtr(),J2ij.ptrB(),J2ij.ptrE());
 
   // normalize by the gyroscopic factor
   for(i=0; i<nspins; ++i) {
