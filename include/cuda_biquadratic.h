408a89cf88bba1c9f990a469b1215c7fa8e209b5
Joe Barker <jb544@york.ac.uk>
2012-01-08 23:22:29 +0000

    Clean up and calculate y = A_ij*y + beta*y

--- include/cuda_biquadratic.h
+++ include/cuda_biquadratic.h
@@ -8,6 +8,7 @@ __global__ void biquadratic_dia_kernel
  const int ncols,
  const int ndiag,
  const int pitch,
+ const float beta,
  const int * dia_offsets,
  const float * dia_values,
  float * sf_dev,
@@ -32,58 +33,64 @@ __global__ void biquadratic_dia_kernel
       // process chunk
       for(int row = thread_id; row < nrows; row += grid_size)
       {
-        int sum[3];
-//         if(base == 0){
-          sum[0] = 0.0;
-          sum[1] = 0.0;
-          sum[2] = 0.0;
-//         } else {
-//            sum[0] = h_dev[3*row+0]; 
-//            sum[1] = h_dev[3*row+1]; 
-//            sum[2] = h_dev[3*row+2];
-//         }
-
-          // index into values array
-          int idxUp  = row + pitch * base;
-  
-          for(int n = 0; n < chunk_size; n++)
-          {
-              const int colUp  = row + offsets[n];
-              const int colLow = row - offsets[n];
-
-              if(colLow >= row && colLow < ncols) {
-                const int si = 3*row;
-                const int sj = 3*colLow;
-
-                const float tmp = sf_dev[si+0]*sf_dev[sj+0] 
-                                + sf_dev[si+1]*sf_dev[sj+1]
-                                + sf_dev[si+2]*sf_dev[sj+2];
-                  
-                const float A_ij = tmp*dia_values[pitch*(base+n)+colLow];
-                sum[0] += A_ij * sf_dev[sj+0];
-                sum[1] += A_ij * sf_dev[sj+1];
-                sum[2] += A_ij * sf_dev[sj+2];
-              }
-              if(colUp >= 0 && colUp < row) {
-                const int si = 3*row;
-                const int sj = 3*colUp;
-
-                const float tmp = sf_dev[si+0]*sf_dev[sj+0] 
-                                + sf_dev[si+1]*sf_dev[sj+1]
-                                + sf_dev[si+2]*sf_dev[sj+2];
-                  
-                const float A_ij = tmp*dia_values[idxUp];
-                sum[0] += A_ij * sf_dev[sj+0];
-                sum[1] += A_ij * sf_dev[sj+1];
-                sum[2] += A_ij * sf_dev[sj+2];
-              }
-
-              idxUp += pitch;
+        const int rowOff = 3*row;
+        float sum[3];
+        
+        if(base == 0){
+          // read initial sum values
+          for(int i=0; i<3; ++i){
+            sum[i] = beta*h_dev[rowOff+i];
           }
-  
-          h_dev[3*row+0] += sum[0];
-          h_dev[3*row+1] += sum[1];
-          h_dev[3*row+2] += sum[2];
+        } else {
+          // outside base 0 use existing values
+          for(int i=0; i<3; ++i){
+            sum[i] = h_dev[rowOff+i];
+          }
+        }
+
+        // index into values array
+        int idxUp  = row + pitch * base;
+
+        for(int n = 0; n < chunk_size; n++)
+        {
+            const int colUp  = row + offsets[n];
+            const int colLow = row - offsets[n];
+
+            if(colLow >= row && colLow < ncols) {
+              const int si = rowOff; // 3*row
+              const int sj = 3*colLow;
+
+              const float prod = sf_dev[si+0]*sf_dev[sj+0] 
+                              + sf_dev[si+1]*sf_dev[sj+1]
+                              + sf_dev[si+2]*sf_dev[sj+2];
+                
+              const float A_ij = prod*dia_values[pitch*(base+n)+colLow];
+
+              for(int i=0; i<3; ++i){
+                sum[i] += A_ij * sf_dev[sj+i];
+              }
+            }
+            if(colUp >= 0 && colUp < row) {
+              const int si = rowOff; // 3*row
+              const int sj = 3*colUp;
+
+              const float prod = sf_dev[si+0]*sf_dev[sj+0] 
+                              + sf_dev[si+1]*sf_dev[sj+1]
+                              + sf_dev[si+2]*sf_dev[sj+2];
+                
+              const float A_ij = prod*dia_values[idxUp];
+              
+              for(int i=0; i<3; ++i){
+                sum[i] += A_ij * sf_dev[sj+i];
+              }
+            }
+
+            idxUp += pitch;
+        }
+
+        for(int i=0; i<3; ++i){
+          h_dev[rowOff+i] = sum[i];
+        }
       }
 
       // wait until all threads are done reading offsets 
