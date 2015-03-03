3c13121a7ee690f4568afcf882053786be1115b4
Joseph Barker <joseph.barker@imr.tohoku.ac.jp>
2015-03-03 19:44:36 +0900

    Create thermostat base class
    The thermostat will now become a virtual class which is implemented concretely in thermostats/. The vitural class includes a factory for creating a definate thermostat.
    
    Signed-off-by: Joseph Barker <joseph.barker@imr.tohoku.ac.jp>

---
+++ core/thermostat.cc
@@ -0,0 +1,27 @@
+// Copyright 2014 Joseph Barker. All rights reserved.
+
+#include <string>
+
+#include "core/globals.h"
+#include "core/thermostat.h"
+
+#include "thermostat/cuda_langevin_white.h"
+
+Thermostat* Thermostat::create(const std::string &thermostat_name) {
+    // debugging output
+    if (::verbose_output_is_set) {
+        ::output.write("\ncreating '%s' thermostat\n", thermostat_name.c_str());
+    }
+
+    // create the selected thermostat
+    if (thermostat_name == "CUDA_LANGEVIN_WHITE") {
+        return new CudaLangevinWhiteThermostat(const double &temperature, const double &sigma, const int num_spins);
+    }
+    // if (thermostat_name == "LANGEVIN_COTH") {
+    //     return new LangevinCothThermostat();
+    // }
+
+    // throw error if the thermostat name is no known
+    jams_error("Unknown thermostat requested '%s'", thermostat_name.c_str());
+    return NULL;
+}
