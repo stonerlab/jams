5cc8f2ceff828acbba1e0f260cc62db6b6b33b0b
Joseph Barker <joseph.barker@imr.tohoku.ac.jp>
2015-08-20 10:00:32 +0900

    Add extra constants
    Signed-off-by: Joseph Barker <joseph.barker@imr.tohoku.ac.jp>

--- core/consts.h
+++ core/consts.h
@@ -7,12 +7,15 @@
 
 const double kZero                  = 0.0;
 const double kOne                   = 1.0;
+const double kTwo                   = 2.0;
+const double kThree                 = 3.0;
 const double kPi                    = 3.14159265358979323846264338327950288;
 const double kTwoPi                 = 2.0*kPi;
 const double kBohrMagneton          = 9.27400915E-24;
 const double kGyromagneticRatio     = 1.760859770e11;
 const double kBoltzmann             = 1.3806504e-23;
-const double kVacuumPermeadbility   = 4*kPi*1E-7;
+const double kVacuumPermeadbility        = 4*kPi*1E-7;
+const double kVacuumPermeadbility_FourPi = 1E-7;
 const double kNanometer             = 1E-9;
 const double kHBar                  = 1.05457173e-34;
 const double kTHz                   = 1E12;
