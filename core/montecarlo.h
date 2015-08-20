a945614d0dba642c7724b9b9ddcc692a79815ecb
Joseph Barker <joseph.barker@imr.tohoku.ac.jp>
2015-08-20 16:03:23 +0900

    Add 90deg rotation trial move to montecarlo.h
    Signed-off-by: Joseph Barker <joseph.barker@imr.tohoku.ac.jp>

--- core/montecarlo.h
+++ core/montecarlo.h
@@ -1,6 +1,8 @@
 #ifndef JAMS_CORE_MONTECARLO_H
 #define JAMS_CORE_MONTECARLO_H
 
+#include "core/consts.h"
+
 // Trial steps as defined in Hinzke Comput. Phys. Commun. 1999
 // RTS
 inline jblib::Vec3<double> mc_reflection_trial_step(jblib::Vec3<double> spin) {
@@ -18,6 +20,13 @@ inline jblib::Vec3<double> mc_small_trial_step(jblib::Vec3<double> spin) {
     spin = spin + mc_uniform_trial_step(spin)*0.1;
     return spin / abs(spin);
 }
+
+// 90deg rotation with random inplane angle PTS
+inline jblib::Vec3<double> mc_perpendicular_trial_step(jblib::Vec3<double> spin) {
+    const double phi = rng.uniform()*kTwoPi;
+    return jblib::Vec3<double>(spin.z, sin(phi)*spin.x + cos(phi)*spin.y, -cos(phi)*spin.x + sin(phi)*spin.y);
+}
+
 
 inline jblib::Vec3<double> mc_spin_as_vec(const int i) {
     return jblib::Vec3<double>(globals::s(i,0), globals::s(i,1), globals::s(i,2));
