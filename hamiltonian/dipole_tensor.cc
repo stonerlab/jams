9f1bbbf1f62b8c3f92c02986c7c50fd71cceaf04
Joseph Barker <joseph.barker@imr.tohoku.ac.jp>
2015-08-28 16:47:52 +0900

    Bugfix: Generalize r_cutoff for larger than simulation cell
    Signed-off-by: Joseph Barker <joseph.barker@imr.tohoku.ac.jp>

--- hamiltonian/dipole_tensor.cc
+++ hamiltonian/dipole_tensor.cc
@@ -12,9 +12,32 @@ DipoleHamiltonianTensor::DipoleHamiltonianTensor(const libconfig::Setting &setti
     using std::pow;
     double r_abs;
     jblib::Vec3<double> r_ij, r_hat, s_j;
-    r_cutoff_ = 32.0;
 
-    output.write("  dipole tensor memory estimate: %fMB", std::pow(double(globals::num_spins*3), 2)*8/double(1024*1024) );
+    jblib::Vec3<int> L_max(0, 0, 0);
+    jblib::Vec3<double> super_cell_dim(0.0, 0.0, 0.0);
+
+    for (int n = 0; n < 3; ++n) {
+        super_cell_dim[n] = 0.5*double(lattice.size(n));
+    }
+
+    r_cutoff_ = *std::max_element(super_cell_dim.begin(), super_cell_dim.end());
+
+    if (settings.exists("r_cutoff")) {
+        r_cutoff_ = settings["r_cutoff"];
+    }
+
+
+    // printf("  super cell max extent (cartesian):\n    %f %f %f\n", super_cell_dim[0], super_cell_dim[1], super_cell_dim[2]);
+
+    for (int n = 0; n < 3; ++n) {
+        if (lattice.is_periodic(n)) {
+            L_max[n] = ceil(r_cutoff_/super_cell_dim[n]);
+        }
+    }
+
+    printf("  image vector max extent (fractional):\n    %d %d %d\n", L_max[0], L_max[1], L_max[2]);
+
+    output.write("  dipole tensor memory estimate (MB):\n    %f\n", std::pow(double(globals::num_spins*3), 2)*8/double(1024*1024) );
 
     dipole_tensor_.resize(globals::num_spins*3, globals::num_spins*3);
 
@@ -31,24 +54,43 @@ DipoleHamiltonianTensor::DipoleHamiltonianTensor(const libconfig::Setting &setti
 
 
     for (int i = 0; i < globals::num_spins; ++i) {
+
         for (int j = 0; j < globals::num_spins; ++j) {
-            if (unlikely(j == i)) continue;
-            r_ij  = lattice.minimum_image(lattice.position(i), lattice.position(j));
-            r_abs = abs(r_ij);
 
-            if (r_abs > r_cutoff_) continue;
+            // loop over periodic images of the simulation lattice
+            // this means r_cutoff can be larger than the simulation cell
+            for (int Lx = -L_max[0]; Lx < L_max[0]+1; ++Lx) {
+                for (int Ly = -L_max[1]; Ly < L_max[1]+1; ++Ly) {
+                    for (int Lz = -L_max[2]; Lz < L_max[2]+1; ++Lz) {
+                        jblib::Vec3<int> image_vector(Lx, Ly, Lz);
 
-            r_hat = r_ij / r_abs;
+                        r_ij  = lattice.generate_image_position(lattice.position(j), image_vector) - lattice.position(i);
 
-            for (int m = 0; m < 3; ++m) {
-                for (int n = 0; n < 3; ++n) {
-                    tensor[m][n] = 3*r_hat[m]*r_hat[n] - Id[m][n];
-                }
-            }
+                        r_abs = abs(r_ij);
 
-            for (int m = 0; m < 3; ++m) {
-                for (int n = 0; n < 3; ++n) {
-                    dipole_tensor_(3*i + m, 3*j + n) = tensor[m][n]*prefactor*globals::mus(j)/pow(r_abs,3);
+                        // i can interact with i in another image of the simulation cell (just not the 0, 0, 0 image)
+                        // so detect based on r_abs rather than i == j
+                        if (r_abs > r_cutoff_ || r_abs < 1e-5) continue;
+
+                        if (i == 0) {
+                        std::cerr << image_vector.x << "\t" << image_vector.y << "\t" << image_vector.z << "\t" <<  r_ij.x << "\t" << r_ij.y << "\t" << r_ij.z << "\t" << lattice.generate_image_position(lattice.position(j), image_vector).x << "\t" << lattice.generate_image_position(lattice.position(j), image_vector).y << "\t" << lattice.generate_image_position(lattice.position(j), image_vector).z << std::endl;
+                        }
+
+
+                        r_hat = r_ij / r_abs;
+
+                        for (int m = 0; m < 3; ++m) {
+                            for (int n = 0; n < 3; ++n) {
+                                tensor[m][n] = 3*r_hat[m]*r_hat[n] - Id[m][n];
+                            }
+                        }
+
+                        for (int m = 0; m < 3; ++m) {
+                            for (int n = 0; n < 3; ++n) {
+                                dipole_tensor_(3*i + m, 3*j + n) += tensor[m][n]*prefactor*globals::mus(j)/pow(r_abs,3);
+                            }
+                        }
+                    }
                 }
             }
         }
