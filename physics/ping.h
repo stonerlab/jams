bf14becd137a4f5a642657035ed39a3c210feedf
Joseph Barker <joseph.barker@imr.tohoku.ac.jp>
2015-10-05 11:09:18 +0900

    Created physics class for "ping" rotations of the spin system

---
+++ physics/ping.h
@@ -0,0 +1,24 @@
+// Copyright 2014 Joseph Barker. All rights reserved.
+
+#ifndef JAMS_PHYSICS_PING_H
+#define JAMS_PHYSICS_PING_H
+
+#include <libconfig.h++>
+
+#include <fstream>
+#include <vector>
+
+#include "core/physics.h"
+
+#include "jblib/containers/array.h"
+
+class PingPhysics : public Physics {
+ public:
+  PingPhysics(const libconfig::Setting &settings);
+  ~PingPhysics();
+  void update(const int &iterations, const double &time, const double &dt);
+ private:
+  bool initialized;
+};
+
+#endif  // JAMS_PHYSICS_PING_H
