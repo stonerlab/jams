e527e4c68c1d4b97d838281077e5eb43935bb0dd
Joseph Barker <joseph.barker@imr.tohoku.ac.jp>
2015-11-05 15:47:25 +0900

    Output spin pumping distributions
    Signed-off-by: Joseph Barker <joseph.barker@imr.tohoku.ac.jp>

--- monitors/spin_pumping.cc
+++ monitors/spin_pumping.cc
@@ -9,6 +9,7 @@
 #include "monitors/spin_pumping.h"
 
 #include <string>
+#include <iomanip>
 
 #include "core/globals.h"
 #include "core/maths.h"
@@ -19,23 +20,35 @@ SpinPumpingMonitor::SpinPumpingMonitor(const libconfig::Setting &settings)
 : Monitor(settings) {
   ::output.write("Initialising Energy Distribution monitor\n");
 
-  std::string name = "_iz_dist.dat";
+  std::string name = "_iz_dist.tsv";
   name = seedname+name;
   iz_dist_file.open(name.c_str());
+  iz_dist_file.setf(std::ios::right);
+  iz_dist_file << std::setw(12) << "bin" << "\t";
+  iz_dist_file << std::setw(12) << "Iz_g_real" << "\t";
+  iz_dist_file << std::setw(12) << "count" << std::endl;
 
-  name = "_iz_mean.dat";
+
+  name = "_iz_mean.tsv";
   name = seedname+name;
   iz_mean_file.open(name.c_str());
+  iz_mean_file.setf(std::ios::right);
+  iz_mean_file << std::setw(12) << "time" << "\t";
+  iz_mean_file << std::setw(12) << "Iz_g_real" << "\t";
+  iz_mean_file << std::setw(12) << "Iz_g_imag" << std::endl;
 
-  name = "_w_dist.dat";
+  name = "_w_dist.tsv";
   name = seedname+name;
   w_dist_file.open(name.c_str());
+  w_dist_file << std::setw(12) << "bin" << "\t";
+  w_dist_file << std::setw(12) << "w_freq" << "\t";
+  w_dist_file << std::setw(12) << "count" << std::endl;
 
 }
 
 void SpinPumpingMonitor::update(Solver * solver) {
   using namespace globals;
-
+  using std::abs;
 
 
   if (solver->iteration()%output_step_freq_ == 0) {
@@ -62,8 +75,11 @@ void SpinPumpingMonitor::update(Solver * solver) {
 
     std::vector<double> energy_total(num_spins, 0.0);
     std::vector<double> pumping_total(num_spins, 0.0);
+    std::vector<double> imaginary_total(num_spins, 0.0);
     std::vector<double> angular_velocity_total(num_spins, 0.0);
 
+    Stats pumping_abs_pos;
+    Stats pumping_abs_neg;
 
     for (int i = 0; i < num_spins; ++i) {
       // double vt[3]= { 0.0,
@@ -71,33 +87,61 @@ void SpinPumpingMonitor::update(Solver * solver) {
       //                 s(i, 0)*ds_dt(i, 1) - s(i, 1)*ds_dt(i, 0) };
 
       pumping_total[i] = ((s(i, 0)*ds_dt(i, 1) - s(i, 1)*ds_dt(i, 0)));
+
+      if (pumping_total[i] > 0.0) {
+        pumping_abs_pos.add(abs(pumping_total[i]));
+      } else {
+        pumping_abs_neg.add(abs(pumping_total[i]));
+      }
+
+      imaginary_total[i] = ds_dt(i, 2);
       angular_velocity_total[i] = ((s(i, 0)*ds_dt(i, 1) - s(i, 1)*ds_dt(i, 0))) / (s(i, 0)*s(i, 0) + s(i, 1)*s(i, 1));
     }
 
-    std::vector<double> range;
-    std::vector<double> bin;
+    // dt_sim = dt * gamma_e
+    // so 1/dt_sm -> 1/dt means x gamma_e
+
+    std::vector<double> range, range_pos, range_neg;
+    std::vector<double> bin, bin_pos, bin_neg;
+
 
     Stats pumping(pumping_total);
+    Stats imaginary(imaginary_total);
 
-    pumping.histogram(range, bin);
+    double p_min_max = std::max(abs(pumping.min()), abs(pumping.max()));
+
+
+    pumping.histogram(range, bin, -p_min_max, p_min_max);
+
+    pumping_abs_pos.histogram(range_pos, bin_pos, -p_min_max, p_min_max, bin.size());
+    pumping_abs_neg.histogram(range_neg, bin_neg, -p_min_max, p_min_max, bin.size());
 
     double norm = 1.0 / pumping_total.size();
     for (int i = 0; i < bin.size(); ++i) {
-      iz_dist_file << i << "\t" << 0.5*(range[i] + range[i+1]) << "\t" << bin[i] * norm << "\n";
+      // output in rad / s^-1 T^-1
+      iz_dist_file << std::setw(12) << i << "\t";
+      iz_dist_file << std::setw(12) << std::scientific << 0.5*(range[i] + range[i+1]) * kGyromagneticRatio << "\t";
+      iz_dist_file << std::setw(12) << std::scientific << bin[i] * norm << "\t";
+      iz_dist_file << std::setw(12) << std::scientific << (bin_pos[i] / pumping.size()) << "\t";
+      iz_dist_file << std::setw(12) << std::scientific << (bin_neg[i] / pumping.size()) << "\n";
+      // iz_dist_file << std::setw(12) << std::scientific << (bin_pos[i]-bin_neg[i]) * norm << "\n";
     }
     iz_dist_file << "\n" << std::endl;
 
-    iz_mean_file << solver->time() << "\t" << pumping.mean() << std::endl;
-
-    std::vector<double> w_range;
-    std::vector<double> w_bin;
+    // output in rad / s^-1 T^-1
+    iz_mean_file << std::setw(12) << std::scientific << solver->time() << "\t";
+    iz_mean_file << std::setw(12) << std::scientific << pumping.mean() * kGyromagneticRatio << "\t";
+    iz_mean_file << std::setw(12) << std::scientific << imaginary.mean() * kGyromagneticRatio << std::endl;
 
     Stats angular_velocity_stats(angular_velocity_total);
-    angular_velocity_stats.histogram(w_range, w_bin);
+    angular_velocity_stats.histogram(range, bin);
 
     norm = 1.0 / angular_velocity_total.size();
-    for (int i = 0; i < w_bin.size(); ++i) {
-      w_dist_file << i << "\t" << 0.5*(w_range[i] + w_range[i+1]) << "\t" << w_bin[i] * norm << "\n";
+    for (int i = 0; i < bin.size(); ++i) {
+      // output in Hz
+      w_dist_file << std::setw(12) << i << "\t";
+      w_dist_file << std::setw(12) << std::scientific << 0.5*(range[i] + range[i+1]) * kGyromagneticRatio << "\t";
+      w_dist_file << std::setw(12) << std::scientific << bin[i] * norm << "\n";
     }
     w_dist_file << "\n" << std::endl;
 
