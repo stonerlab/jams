85d33608844209c828a7455e4321e8d511166034
Joseph Barker <joseph.barker@imr.tohoku.ac.jp>
2015-11-09 13:51:58 +0900

    Calculate chi_xy for dsf
    Signed-off-by: Joseph Barker <joseph.barker@imr.tohoku.ac.jp>

--- monitors/structurefactor.h
+++ monitors/structurefactor.h
@@ -35,6 +35,8 @@ class StructureFactorMonitor : public Monitor {
   jblib::Array<std::complex<double>, 3> sqw_x;
   jblib::Array<std::complex<double>, 3> sqw_y;
   jblib::Array<std::complex<double>, 3> sqw_z;
+  jblib::Array<fftw_complex, 1> chi_xy;
+  jblib::Array<fftw_complex, 1> chi_yx;
   std::vector<jblib::Vec3<int> > bz_points;
   std::vector<double> bz_lengths;
   double delta_freq_;
