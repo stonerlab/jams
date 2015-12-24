59faba52cfc8bda7e51972e65f413f427bef4c67
Joe Barker <joebarker87@gmail.com>
2015-12-24 08:50:55 +0000

    Basic working Wolff MC algorithm
    Signed-off-by: Joe Barker <joebarker87@gmail.com>

--- solvers/monte-carlo-wolff.h
+++ solvers/monte-carlo-wolff.h
@@ -5,14 +5,24 @@
 
 #include "core/solver.h"
 
+#include <queue>
+#include <vector>
+
+typedef std::vector<std::vector<int>> adjacency_list;
+
 class MonteCarloWolffSolver : public Solver {
  public:
   MonteCarloWolffSolver();
   ~MonteCarloWolffSolver();
+
   void initialize(int argc, char **argv, double dt);
   void run();
+  int cluster_move();
 
  private:
+   int attempted_moves_;
+   int cluster_size_;
+   adjacency_list    neighbours_;
 };
 
 #endif  // JAMS_SOLVER_MONTE_CARLO_WOLFF_H
