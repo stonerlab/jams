4596a0a65062fc7291f8876a5e155c10cc13d606
Joseph Barker <joseph.barker@imr.tohoku.ac.jp>
2016-01-18 12:41:59 +0900

    Add electron g factor to constants
    Signed-off-by: Joseph Barker <joseph.barker@imr.tohoku.ac.jp>

--- core/consts.h
+++ core/consts.h
@@ -20,6 +20,7 @@ const double kBohrMagneton          = 9.27400915E-24;
 const double kGyromagneticRatio     = 1.760859770e11;
 const double kBoltzmann             = 1.3806504e-23;
 const double kVacuumPermeadbility        = 4*kPi*1E-7;
+const double kElectronGFactor       = 2.00231930436182; //         || NIST 2014 CODATA
 const double kNanometer             = 1E-9;
 const double kHBar                  = 1.05457173e-34;
 const double kTHz                   = 1E12;
