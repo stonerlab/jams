da78d0e7bcb71be8949ae70ab41303d654fcf23c
Joseph Barker <joseph.barker@imr.tohoku.ac.jp>
2016-04-08 13:59:00 +0900

    Calculate SMR quantities of each spin
    Signed-off-by: Joseph Barker <joseph.barker@imr.tohoku.ac.jp>

--- monitors/smr.cc
+++ monitors/smr.cc
@@ -27,8 +27,12 @@ SMRMonitor::SMRMonitor(const libconfig::Setting &settings)
   outfile << std::setw(12) << "time" << "\t";
 
   for (int i = 0; i < lattice.num_materials(); ++i) {
-    outfile << std::setw(12) << lattice.material_name(i) + ":mj" << "\t";
-    outfile << std::setw(12) << lattice.material_name(i) + ":mt" << "\t";
+    outfile << std::setw(12) << lattice.material_name(i) + ":mtsq_para" << "\t";
+    outfile << std::setw(12) << lattice.material_name(i) + ":mtsq_perp" << "\t";
+
+    outfile << std::setw(12) << lattice.material_name(i) + ":mjmt_para" << "\t";
+    outfile << std::setw(12) << lattice.material_name(i) + ":mjmt_perp" << "\t";
+
     outfile << std::setw(12) << lattice.material_name(i) + ":mn" << "\t";
   }
 
@@ -38,8 +42,12 @@ SMRMonitor::SMRMonitor(const libconfig::Setting &settings)
 void SMRMonitor::update(Solver * solver) {
   using namespace globals;
 
-  std::vector<double> mj(lattice.num_materials(), 0.0);
-  std::vector<double> mt(lattice.num_materials(), 0.0);
+  std::vector<double> mtsq_para(lattice.num_materials(), 0.0);
+  std::vector<double> mtsq_perp(lattice.num_materials(), 0.0);
+
+  std::vector<double> mjmt_para(lattice.num_materials(), 0.0);
+  std::vector<double> mjmt_perp(lattice.num_materials(), 0.0);
+
   std::vector<double> mn(lattice.num_materials(), 0.0);
 
   for (int i = 0; i < num_spins; ++i) {
@@ -49,21 +57,30 @@ void SMRMonitor::update(Solver * solver) {
     // t -> y
     // n -> z
     int type = lattice.atom_material(i);
-    mj[type] += s(i, 0);
-    mt[type] += s(i, 1);
-    mn[type] += s(i, 2);
+    mtsq_para[type] +=  s(i, 1) * s(i, 1);
+    mtsq_perp[type] +=  s(i, 0) * s(i, 0);
+    mjmt_para[type] +=  s(i, 0) * s(i, 1);
+    mjmt_perp[type] += -s(i, 0) * s(i, 1);
+
+    mn[type]   += s(i, 2);
   }
 
   for (int i = 0; i < lattice.num_materials(); ++i) {
-      mj[i] = mj[i]/static_cast<double>(lattice.num_of_material(i));
-      mt[i] = mt[i]/static_cast<double>(lattice.num_of_material(i));
+      mtsq_para[i] = mtsq_para[i]/static_cast<double>(lattice.num_of_material(i));
+      mtsq_perp[i] = mtsq_perp[i]/static_cast<double>(lattice.num_of_material(i));
+
+      mjmt_para[i] = mjmt_para[i]/static_cast<double>(lattice.num_of_material(i));
+      mjmt_perp[i] = mjmt_perp[i]/static_cast<double>(lattice.num_of_material(i));
+
       mn[i] = mn[i]/static_cast<double>(lattice.num_of_material(i));
   }
 
   outfile << std::setw(12) << std::scientific << solver->time() << "\t";
 
   for (int i = 0; i < lattice.num_materials(); ++i) {
-    outfile << std::setw(12) << mj[i] << "\t" << mt[i] << "\t" << mn[i] << "\t";
+    outfile << std::setw(12) << mtsq_para[i] << "\t" << mtsq_perp[i] << "\t";
+    outfile << std::setw(12) << mjmt_para[i] << "\t" << mjmt_perp[i] << "\t";
+    outfile << std::setw(12) << mn[i] << "\t";
   }
 
   outfile << std::endl;
