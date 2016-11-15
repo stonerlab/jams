149fdcd6ddb03f82b64430795c4c79617334b48f
Joseph Barker <jbarker@mallory.imr.tohoku.ac.jp>
2016-11-15 20:45:02 +0900

    Major optimizations of GPU bruteforce dipole

--- hamiltonian/dipole_bruteforce_kernel.h
+++ hamiltonian/dipole_bruteforce_kernel.h
@@ -9,7 +9,7 @@ __constant__ bool   dev_super_cell_pbc[3];
 __constant__ float dev_dipole_prefactor;
 __constant__ float dev_r_cutoff;
 
-constexpr int shared_size = 256;
+constexpr int shared_size = 128;
 
 __device__ inline void displacement(
     const float ri[3],
@@ -56,67 +56,20 @@ __device__ inline void displacement(
           + dev_super_unit_cell[2][2] * dr_frac[2];
 }
 
-// __global__ void dipole_bruteforce_kernel
-// (
-//     const double * s_dev,
-//     cudaTextureObject_t r_tex,
-//     const float * mus_dev,
-//     const unsigned int num_spins,
-//     double * h_dev
-// )
-// {
-//     const unsigned int idx = blockIdx.x*blockDim.x+threadIdx.x;
-//     unsigned int n;
+__device__ void field_element(const float sj[3], const float r_ij[3], const float pre, const float r_abs_sq, float h[3]) {
 
-//     double h[3] = {0.0, 0.0, 0.0};
+  const float d_r_abs = rsqrtf(r_abs_sq);
 
-//     float sj[3], ri[3], rj[3], r_ij[3], r_abs, w0, sj_dot_rhat;
+  const float sj_dot_rhat = 3.0f * (sj[0] * r_ij[0] + sj[1] * r_ij[1] + sj[2] * r_ij[2]);
 
-//     if (idx < num_spins) {
-//         // constant is kB * mu_0 / 4 pi
-//         const float pre = mus_dev[idx] * dev_dipole_prefactor;
+  const float w0 = pre * d_r_abs * d_r_abs * d_r_abs * d_r_abs * d_r_abs;
 
-//         for (n = 0; n < 3; ++n) {
-//             ri[n] = tex1Dfetch<float>(r_tex, 3*idx + n);
-//         }
+  h[0] = w0 * (r_ij[0] * sj_dot_rhat  - r_abs_sq * sj[0]);
+  h[1] = w0 * (r_ij[1] * sj_dot_rhat  - r_abs_sq * sj[1]);
+  h[2] = w0 * (r_ij[2] * sj_dot_rhat  - r_abs_sq * sj[2]);
+}
 
-//         for (int j = 0; j < num_spins; ++j) {
-//           // __syncthreads();
-
-//           for (n = 0; n < 3; ++n) {
-//               rj[n] = tex1Dfetch<float>(r_tex, 3*j + n);
-//           }
-
-//           for (n = 0; n < 3; ++n) {
-//               sj[n] = s_dev[3*j + n];
-//           }
-
-//           displacement(ri, rj, r_ij);
-
-//           r_abs = r_ij[0] * r_ij[0] + r_ij[1] * r_ij[1] + r_ij[2] * r_ij[2];
-
-//           if (j == idx || r_abs > (dev_r_cutoff * dev_r_cutoff)) continue;
-
-//           r_abs = rsqrtf(r_abs);
-
-//           w0 = pre * mus_dev[j] * (r_abs * r_abs * r_abs);
-
-//           sj_dot_rhat = 3.0f * (sj[0] * r_ij[0] + sj[1] * r_ij[1] + sj[2] * r_ij[2]) * r_abs * r_abs;
-
-//           // accumulate values
-//           for (n = 0; n < 3; ++n) {
-//               h[n] += (r_ij[n] * sj_dot_rhat  - sj[n]) * w0;
-//           }
-//         }
-
-//         // write to global memory
-//         #pragma unroll
-//         for (n = 0; n < 3; ++n) {
-//             h_dev[3*idx + n] = h[n];
-//         }
-//     }
-// }
-
+/*
 __global__ void dipole_bruteforce_kernel
 (
     const double * s_dev,
@@ -127,11 +80,68 @@ __global__ void dipole_bruteforce_kernel
 )
 {
     const unsigned int idx = blockIdx.x*blockDim.x+threadIdx.x;
+    unsigned int n;
+
+    double h[3] = {0.0, 0.0, 0.0};
+
+    float sj[3], ri[3], rj[3], r_ij[3], r_abs, sj_dot_rhat;
+
+    if (idx < num_spins) {
+        // constant is kB * mu_0 / 4 pi
+        const float pre = mus_dev[idx] * dev_dipole_prefactor;
+
+        for (n = 0; n < 3; ++n) {
+            ri[n] = r_dev[3*idx + n];
+        }
+
+        for (int j = 0; j < num_spins; ++j) {
+
+          for (n = 0; n < 3; ++n) {
+              rj[n] = r_dev[3*j + n];
+          }
+
+          for (n = 0; n < 3; ++n) {
+              sj[n] = s_dev[3*j + n];
+          }
+
+          displacement(ri, rj, r_ij);
+
+          r_abs = sqrtf(r_ij[0] * r_ij[0] + r_ij[1] * r_ij[1] + r_ij[2] * r_ij[2]);
+
+          if (j == idx || r_abs > dev_r_cutoff) continue;
+
+          sj_dot_rhat = (sj[0] * r_ij[0] + sj[1] * r_ij[1] + sj[2] * r_ij[2]);
+
+          // accumulate values
+          for (n = 0; n < 3; ++n) {
+              h[n] += pre * mus_dev[j] * (3.0f * r_ij[n] * sj_dot_rhat  - r_abs * r_abs * sj[n]) / (r_abs * r_abs * r_abs * r_abs * r_abs);
+          }
+        }
+
+        // write to global memory
+        #pragma unroll
+        for (n = 0; n < 3; ++n) {
+            h_dev[3*idx + n] = h[n];
+        }
+    }
+}
+*/
+
+__global__ void dipole_bruteforce_kernel
+(
+    const double * __restrict__ s_dev,
+    const float * __restrict__ r_dev,
+    const float * __restrict__ mus_dev,
+    const unsigned int num_spins,
+    double * __restrict__ h_dev
+)
+{
+    const unsigned int idx = blockIdx.x*blockDim.x+threadIdx.x;
     const unsigned int tx = threadIdx.x;
 
     __shared__ float rj[shared_size][3];
     __shared__ float sj[shared_size][3];
-    __shared__ float mus[shared_size];
+    __shared__ float mus_pre[shared_size];
 
     if (idx < num_spins) {
         // constant is kB * mu_0 / 4 pi
@@ -148,45 +158,87 @@ __global__ void dipole_bruteforce_kernel
 
           if (shared_idx < num_spins) {
             
-            mus[tx] = mus_dev[shared_idx];
+            mus_pre[tx] = pre * mus_dev[shared_idx];
 
+            #pragma unroll
             for (unsigned int n = 0; n < 3; ++n) {
                 rj[tx][n] = r_dev[3*shared_idx + n];
             }
+
+            #pragma unroll
             for (unsigned int n = 0; n < 3; ++n) {
                 sj[tx][n] = s_dev[3*shared_idx + n];
             }
 
             __syncthreads();
-            for (unsigned int t = 0; t < shared_size; ++t) {
 
-              float r_ij[3];
-              displacement(ri, rj[t], r_ij);
+            // TODO: catch cases where the system size is not a multiple of the block/4
 
-              float r_abs = r_ij[0] * r_ij[0] + r_ij[1] * r_ij[1] + r_ij[2] * r_ij[2];
+            for (unsigned int t = 0; t < shared_size/4; ++t) {
 
-              if (r_abs > dev_r_cutoff * dev_r_cutoff || idx == (shared_size * b + t)) continue;
+              const int t1 = 4*t;
+              const int t2 = 4*t+1;
+              const int t3 = 4*t+2;
+              const int t4 = 4*t+3;
 
-              r_abs = rsqrtf(r_abs);
+              float r_ij1[3], r_ij2[3], r_ij3[3], r_ij4[3];
 
-              const float w0 = pre * mus[t] * (r_abs * r_abs * r_abs);
 
-              const float sj_dot_rhat = 3.0f * (sj[t][0] * r_ij[0] + sj[t][1] * r_ij[1] + sj[t][2] * r_ij[2]) * r_abs * r_abs;
+              const float pre1 = mus_pre[t1];
+              const float pre2 = mus_pre[t2];
+              const float pre3 = mus_pre[t3];
+              const float pre4 = mus_pre[t4];
 
-              // accumulate values
-              #pragma unroll
-              for (unsigned int n = 0; n < 3; ++n) {
-                  h[n] += (r_ij[n] * sj_dot_rhat  - sj[t][n]) * w0;
+
+              displacement(ri, rj[t1], r_ij1);
+              const float r_abs1 = (r_ij1[0] * r_ij1[0] + r_ij1[1] * r_ij1[1] + r_ij1[2] * r_ij1[2]);
+
+              displacement(ri, rj[t2], r_ij2);
+              const float r_abs2 = (r_ij2[0] * r_ij2[0] + r_ij2[1] * r_ij2[1] + r_ij2[2] * r_ij2[2]);
+
+              displacement(ri, rj[t3], r_ij3);
+              const float r_abs3 = (r_ij3[0] * r_ij3[0] + r_ij3[1] * r_ij3[1] + r_ij3[2] * r_ij3[2]);
+
+              displacement(ri, rj[t4], r_ij4);
+              const float r_abs4 = (r_ij4[0] * r_ij4[0] + r_ij4[1] * r_ij4[1] + r_ij4[2] * r_ij4[2]);
+
+
+              float h1[3] = {0.0, 0.0, 0.0};
+              float h2[3] = {0.0, 0.0, 0.0};
+              float h3[3] = {0.0, 0.0, 0.0};
+              float h4[3] = {0.0, 0.0, 0.0};
+
+              if (r_abs1 <= (dev_r_cutoff * dev_r_cutoff) && idx != (shared_size * b + t1)) {
+                field_element(sj[t1], r_ij1, pre1, r_abs1, h1);
               }
+
+              if (r_abs2 <= (dev_r_cutoff * dev_r_cutoff) && idx != (shared_size * b + t2)) {
+                field_element(sj[t2], r_ij2, pre2, r_abs2, h2);
               }
+
+              if (r_abs3 <= (dev_r_cutoff * dev_r_cutoff) && idx != (shared_size * b + t3)) {
+                field_element(sj[t3], r_ij3, pre3, r_abs3, h3);
+              }
+
+              if (r_abs4 <= (dev_r_cutoff * dev_r_cutoff) && idx != (shared_size * b + t4)) {
+                field_element(sj[t4], r_ij4, pre4, r_abs4, h4);
+              }
+
+              h[0] = h[0] + h1[0] + h2[0] + h3[0] + h4[0];
+              h[1] = h[1] + h1[1] + h2[1] + h3[1] + h4[1];
+              h[2] = h[2] + h1[2] + h2[2] + h3[2] + h4[2];
             }
+
+
           }
-        // write to global memory
-        #pragma unroll
-        for (unsigned int n = 0; n < 3; ++n) {
-            h_dev[3*idx + n] = h[n];
         }
+    // write to global memory
+    #pragma unroll
+    for (unsigned int n = 0; n < 3; ++n) {
+        h_dev[3*idx + n] = h[n];
     }
+  } 
 }
+
 
 #endif  // JAMS_HAMILTONIAN_DIPOLE_BRUTEFORCE_KERNEL_H
