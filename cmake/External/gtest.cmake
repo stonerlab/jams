7efb5271e2c4feccd6f7eff6949a81895442a15c
Joseph Barker <joseph.barker@imr.tohoku.ac.jp>
2017-03-26 18:43:54 +0900

    Make sure gtest is downloaded with —depth=1

--- cmake/External/gtest.cmake
+++ cmake/External/gtest.cmake
@@ -9,6 +9,7 @@ include(cmake/DownloadProject/DownloadProject.cmake)
 download_project(PROJ                googletest
                  GIT_REPOSITORY      https://github.com/google/googletest.git
                  GIT_TAG             master
+                 GIT_SHALLOW         1
                  ${UPDATE_DISCONNECTED_IF_AVAILABLE}
 )
 
