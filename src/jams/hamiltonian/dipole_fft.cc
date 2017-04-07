05ab3828c59acc92544af7f06017a4a393ba51d6
Joseph Barker <joseph.barker@imr.tohoku.ac.jp>
2017-04-07 22:14:01 +0900

    General cleanup of dipole_fft code

--- src/jams/hamiltonian/dipole_fft.cc
+++ src/jams/hamiltonian/dipole_fft.cc
@@ -35,13 +35,8 @@ DipoleHamiltonianFFT::DipoleHamiltonianFFT(const libconfig::Setting &settings, c
     jblib::Matrix<double, 3, 3> Id( 1, 0, 0, 0, 1, 0, 0, 0, 1 );
 
     jblib::Vec3<int> L_max(0, 0, 0);
-    jblib::Vec3<double> super_cell_dim(0.0, 0.0, 0.0);
 
-    for (int n = 0; n < 3; ++n) {
-        super_cell_dim[n] = 0.5*double(lattice->size(n));
-    }
-
-    r_cutoff_ = settings["r_cutoff"];
+    r_cutoff_ = double(settings["r_cutoff"]);
 
     printf("    r_cutoff: %f\n", r_cutoff_);
 
@@ -51,8 +46,6 @@ DipoleHamiltonianFFT::DipoleHamiltonianFFT(const libconfig::Setting &settings, c
     }
     printf("    k_cutoff: %d\n", k_cutoff_);
 
-    const double prefactor = kVacuumPermeadbility*kBohrMagneton/(4*kPi*pow(::lattice->parameter(),3));
-
     for (int n = 0; n < 3; ++n) {
         kspace_size_[n] = ::lattice->num_unit_cells(n);
     }
@@ -60,9 +53,9 @@ DipoleHamiltonianFFT::DipoleHamiltonianFFT(const libconfig::Setting &settings, c
     kspace_padded_size_ = kspace_size_;
 
     for (int n = 0; n < 3; ++n) {
-        if (!::lattice->is_periodic(n)) {
+        // if (!::lattice->is_periodic(n)) {
             kspace_padded_size_[n] = 2*::lattice->num_unit_cells(n);
-        }
+        // }
     }
 
     jblib::Array<double, 5> wij(kspace_padded_size_[0],
@@ -71,45 +64,50 @@ DipoleHamiltonianFFT::DipoleHamiltonianFFT(const libconfig::Setting &settings, c
                                 3,
                                 3);
 
-    std::fill(wij.data(), wij.data()+wij.elements(), 0.0);
+    wij.zero();
 
-    for (int i = 0; i < kspace_padded_size_.x; ++i) {
-        for (int j = 0; j < kspace_padded_size_.y; ++j) {
-            for (int k = 0; k < kspace_padded_size_.z; ++k) {
-                if (unlikely(i == 0 && j == 0 && k == 0)) continue;
+    const double dipole_prefactor = kVacuumPermeadbility*kBohrMagneton /(4*kPi*::lattice->parameter() * ::lattice->parameter() * ::lattice->parameter());
 
-                pos = jblib::Vec3<int>(i, j, k);
+        const int i = 0;
+        const double prefactor = globals::mus(i) * dipole_prefactor;
+        for (int j = 0; j < globals::num_spins; ++j) {
+            if (j == i) continue;
 
-                for (int n = 0; n < 3; ++n) {
-                    if (pos[n] > kspace_padded_size_[n]/2) {
-                        pos[n] = periodic_shift(pos[n], kspace_padded_size_[n]/2) - kspace_padded_size_[n]/2;
-                    }
-                }
-                r_ij = jblib::Vec3<double>(pos.x, pos.y, pos.z);
-                r_ij  = ::lattice->fractional_to_cartesian(r_ij);
+            auto r_ij = lattice->displacement(i, j);
 
-                // r_ij = ::lattice->minimum_image(jblib::Vec3<double>(0, 0, 0), ::lattice->fractional_to_cartesian(pos));
-                r_abs = abs(r_ij);
+            const auto r_abs_sq = r_ij.norm_sq();
 
-                if (r_abs > r_cutoff_ || unlikely(r_abs < 1e-5)) continue;
+            if (r_abs_sq > (r_cutoff_*r_cutoff_)) continue;
+            
+            const auto r_abs = sqrt(r_abs_sq);
 
-                eij = r_ij / r_abs;
+            // divide by product(kspace_padded_size_) instead or normalizing the FFT result later
+            const auto w0 = prefactor / (r_abs_sq * r_abs_sq * r_abs * product(kspace_padded_size_));
+            
+            auto pos = ::lattice->super_cell_pos(j);
 
-                for (int m = 0; m < 3; ++m) {
-                    for (int n = 0; n < 3; ++n) {
-                        // divide by product(kspace_padded_size_) instead or normalizing the FFT result later
-                        wij(i, j, k, m, n) += globals::mus(0)*prefactor*(3*eij[m]*eij[n] - Id[m][n])/double(pow(r_abs, 3)*product(kspace_padded_size_));
-                    }
+            for (int n = 0; n < 3; ++n) {
+                if (pos[n] > kspace_size_[n]/2) {
+                    pos[n] = (kspace_size_[n] + pos[n]);
                 }
             }
+
+            // std::cerr << pos.x << "\t" << pos.y << "\t" << pos.z << "\t" <<r_ij[0] << "\t" << r_ij[1] << "\t" << r_ij[2] << "\n";
+
+            assert(pos[0] < kspace_padded_size_[0]);
+            assert(pos[1] < kspace_padded_size_[1]);
+            assert(pos[2] < kspace_padded_size_[2]);
+
+            for (int m = 0; m < 3; ++m) {
+                for (int n = 0; n < 3; ++n) {
+                    wij(pos.x, pos.y, pos.z, m, n) += w0 * (3.0 * r_ij[m] * r_ij[n] - r_abs_sq * Id[m][n]);
+                }
+            }
+
+
         }
-    }
 
     // --------------------------------------------------------------------------
-
-
-
-
 
     h_nonlocal_.resize(kspace_padded_size_[0], kspace_padded_size_[1], kspace_padded_size_[2], 3);
     s_nonlocal_.resize(kspace_padded_size_[0], kspace_padded_size_[1], kspace_padded_size_[2], 3);
@@ -127,17 +125,26 @@ DipoleHamiltonianFFT::DipoleHamiltonianFFT(const libconfig::Setting &settings, c
 
     printf("    planning FFTs\n");
 
-    int rank = 3;
+    int rank = 3;           
     int stride = 3;
     int dist = 1;
     int num_transforms = 3;
     int * nembed = NULL;
 
     spin_fft_forward_transform_
-        = fftw_plan_many_dft_r2c(rank, &kspace_padded_size_[0], num_transforms,
-                                 s_nonlocal_.data(),  nembed, stride, dist,
-                                 s_recip_.data(), nembed, stride, dist,
-                                 FFTW_PATIENT|FFTW_PRESERVE_INPUT);
+        = fftw_plan_many_dft_r2c(
+            rank,                       // dimensionality
+            &kspace_padded_size_[0],    // array of sizes of each dimension
+            num_transforms,             // number of transforms
+            s_nonlocal_.data(),         // input: real data
+            nembed,                     // number of embedded dimensions 
+            stride,                     // memory stride between elements of one fft dataset 
+            dist,                       // memory distance between fft datasets
+            s_recip_.data(),            // output: complex data
+            nembed,                     // number of embedded dimensions
+            stride,                     // memory stride between elements of one fft dataset 
+            dist,                       // memory distance between fft datasets
+            FFTW_PATIENT|FFTW_PRESERVE_INPUT);
 
     field_fft_backward_transform_
         = fftw_plan_many_dft_c2r(rank, &kspace_padded_size_[0], num_transforms,
@@ -156,8 +163,6 @@ DipoleHamiltonianFFT::DipoleHamiltonianFFT(const libconfig::Setting &settings, c
             w_recip_.data(), nembed, stride, dist,
             FFTW_ESTIMATE|FFTW_PRESERVE_INPUT);
 
-    printf("    transform interaction matrix\n");
-
     fftw_execute(interaction_fft_transform_);
 }
 
@@ -170,12 +175,12 @@ double DipoleHamiltonianFFT::calculate_total_energy() {
     calculate_nonlocal_field();
     for (int i = 0; i < globals::num_spins; ++i) {
         pos = ::lattice->super_cell_pos(i);
-       e_total += -(globals::s(i,0)*h_nonlocal_(pos.x, pos.y, pos.z, 0)
+       e_total += (    globals::s(i,0)*h_nonlocal_(pos.x, pos.y, pos.z, 0)
                      + globals::s(i,1)*h_nonlocal_(pos.x, pos.y, pos.z, 1)
                      + globals::s(i,2)*h_nonlocal_(pos.x, pos.y, pos.z, 2))*globals::mus(i);
     }
 
-    return e_total;
+    return -0.5*e_total;
 }
 
 // --------------------------------------------------------------------------
@@ -256,9 +261,7 @@ void DipoleHamiltonianFFT::calculate_nonlocal_field() {
         s_old_ = globals::s;
     }
 
-    for (int i = 0, iend = s_recip_.elements(); i < iend; ++i) {
-        s_nonlocal_[i];
-    }
+    s_nonlocal_.zero();
 
     for (int i = 0; i < globals::num_spins; ++i) {
          pos = ::lattice->super_cell_pos(i);
@@ -275,21 +278,15 @@ void DipoleHamiltonianFFT::calculate_nonlocal_field() {
     fftw_execute(spin_fft_forward_transform_);
 
     // perform convolution as multiplication in fourier space
-
-    for (i = 0, iend = min(kspace_padded_size_[0], k_cutoff_); i < iend; ++i) {
-      for (j = 0, jend = min(kspace_padded_size_[1], k_cutoff_); j < jend; ++j) {
-        for (k = 0, kend = min(kspace_padded_size_[2]/2+1, k_cutoff_); k < kend; ++k) {
-            if (i*i + j*j + k*k < k_cutoff_*k_cutoff_) {
-                for(m = 0; m < 3; ++m) {
-                    h_recip_(i,j,k,m)[0] = ( w_recip_(i,j,k,m,0)[0]*s_recip_(i,j,k,0)[0]-w_recip_(i,j,k,m,0)[1]*s_recip_(i,j,k,0)[1] )
-                                         + ( w_recip_(i,j,k,m,1)[0]*s_recip_(i,j,k,1)[0]-w_recip_(i,j,k,m,1)[1]*s_recip_(i,j,k,1)[1] )
-                                         + ( w_recip_(i,j,k,m,2)[0]*s_recip_(i,j,k,2)[0]-w_recip_(i,j,k,m,2)[1]*s_recip_(i,j,k,2)[1] );
-
-                    h_recip_(i,j,k,m)[1] = ( w_recip_(i,j,k,m,0)[0]*s_recip_(i,j,k,0)[1]+w_recip_(i,j,k,m,0)[1]*s_recip_(i,j,k,0)[0] )
-                                         + ( w_recip_(i,j,k,m,1)[0]*s_recip_(i,j,k,1)[1]+w_recip_(i,j,k,m,1)[1]*s_recip_(i,j,k,1)[0] )
-                                         + ( w_recip_(i,j,k,m,2)[0]*s_recip_(i,j,k,2)[1]+w_recip_(i,j,k,m,2)[1]*s_recip_(i,j,k,2)[0] );
-                }
-            }
+    for (i = 0; i < kspace_padded_size_[0]; ++i) {
+      for (j = 0; j < kspace_padded_size_[1]; ++j) {
+        for (k = 0; k < (kspace_padded_size_[2]/2)+1; ++k) {
+          for (int m = 0; m < 3; ++ m) {
+            for (int n = 0; n < 3; ++ n) {
+              h_recip_(i,j,k,m)[0] += w_recip_(i,j,k,m,n)[0]*s_recip_(i,j,k,n)[0]-w_recip_(i,j,k,m,n)[1]*s_recip_(i,j,k,n)[1];
+              h_recip_(i,j,k,m)[1] += w_recip_(i,j,k,m,n)[0]*s_recip_(i,j,k,n)[1]+w_recip_(i,j,k,m,n)[1]*s_recip_(i,j,k,n)[0];
+            }   
+          }
         }
       }
     }
