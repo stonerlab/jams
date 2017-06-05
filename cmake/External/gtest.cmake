74c38ff685b617ed9a94ebe3056d2b241fd8a8b9
Joseph Barker <joseph.barker@imr.tohoku.ac.jp>
2017-06-05 18:59:22 +0900

    remove gtest git shallow command

--- cmake/External/gtest.cmake
+++ cmake/External/gtest.cmake
@@ -9,7 +9,6 @@ include(${PROJECT_SOURCE_DIR}/cmake/DownloadProject/DownloadProject.cmake)
 download_project(PROJ                googletest
                  GIT_REPOSITORY      https://github.com/google/googletest.git
                  GIT_TAG             master
-                 GIT_SHALLOW         1
                  ${UPDATE_DISCONNECTED_IF_AVAILABLE}
 )
 
