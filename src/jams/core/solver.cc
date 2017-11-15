bea0e1ecc52625325cafd78d2a6628b8c568132d
Joseph Barker <joseph.barker@imr.tohoku.ac.jp>
2017-11-15 15:50:55 +0900

    use c++11 in solver

--- src/jams/core/solver.cc
+++ src/jams/core/solver.cc
@@ -5,7 +5,6 @@
 
 #include "jams/core/error.h"
 #include "jams/core/physics.h"
-#include "jams/core/types.h"
 #include "jblib/containers/array.h"
 #include "jams/core/blas.h"
 #include "jams/core/solver.h"
@@ -22,56 +21,43 @@
 #include "jams/solvers/constrainedmc.h"
 #include "jams/solvers/cuda_constrainedmc.h"
 
-#ifdef MKL
-#include <mkl_spblas.h>
-#endif
-
 Solver::~Solver() {
-  for (int i = 0, iend = monitors_.size(); i < iend; ++i) {
-    delete monitors_[i];
+  for (auto& m : monitors_) {
+    if (m) {
+      delete m;
+      m = nullptr;
+    }
   }
 }
 
-//---------------------------------------------------------------------
 
 void Solver::initialize(int argc, char **argv, double idt) {
-  using namespace globals;
-  if (initialized_ == true) {
-    jams_error("Solver is already initialized");
-  }
-
+  assert(!initialized_);
   real_time_step_ = idt;
-  time_step_ = idt*kGyromagneticRatio;
-
+  time_step_ = idt * kGyromagneticRatio;
   initialized_ = true;
 }
 
-//---------------------------------------------------------------------
 
 void Solver::run() {
 }
 
-//---------------------------------------------------------------------
 
 void Solver::compute_fields() {
-  using namespace globals;
+  globals::h.zero();
 
-  // zero the effective field array
-  std::fill(h.data(), h.data()+num_spins3, 0.0);
-
-  // calculate each hamiltonian term's fields
-  for (std::vector<Hamiltonian*>::iterator it = hamiltonians_.begin() ; it != hamiltonians_.end(); ++it) {
-    (*it)->calculate_fields();
+  for (auto& hh : hamiltonians_) {
+    hh->calculate_fields();
   }
 
   // sum hamiltonian field contributions into effective field
-  for (std::vector<Hamiltonian*>::iterator it = hamiltonians_.begin() ; it != hamiltonians_.end(); ++it) {
-    cblas_daxpy(num_spins3, 1.0, (*it)->ptr_field(), 1, h.data(), 1);
+  for (auto& hh : hamiltonians_) {
+    cblas_daxpy(globals::num_spins3, 1.0, hh->ptr_field(), 1, globals::h.data(), 1);
   }
 }
 
-Solver* Solver::create(const std::string &solver_name) {
 
+Solver* Solver::create(const std::string &solver_name) {
   if (capitalize(solver_name) == "LLG-HEUN-CPU" || capitalize(solver_name) == "HEUNLLG") {
     return new HeunLLGSolver;
   }
@@ -94,46 +80,42 @@ Solver* Solver::create(const std::string &solver_name) {
 #endif
 
   jams_error("Unknown solver '%s' selected.", solver_name.c_str());
-  return NULL;
+  return nullptr;
 }
 
-//---------------------------------------------------------------------
 
 void Solver::register_physics_module(Physics* package) {
     physics_module_ = package;
 }
 
-//---------------------------------------------------------------------
 
 void Solver::update_physics_module() {
     physics_module_->update(iteration_, time(), time_step_);
 }
 
-//---------------------------------------------------------------------
 
 void Solver::register_monitor(Monitor* monitor) {
   monitors_.push_back(monitor);
 }
 
-//---------------------------------------------------------------------
 
 void Solver::register_hamiltonian(Hamiltonian* hamiltonian) {
   hamiltonians_.push_back(hamiltonian);
 }
 
-//---------------------------------------------------------------------
 
 void Solver::notify_monitors() {
-  for (std::vector<Monitor*>::iterator it = monitors_.begin() ; it != monitors_.end(); ++it) {
-    if((*it)->is_updating(iteration_)){
-      (*it)->update(this);
+  for (auto& m : monitors_) {
+    if (m->is_updating(iteration_)) {
+      m->update(this);
     }
   }
 }
 
+
 bool Solver::is_converged() {
-  for (std::vector<Monitor*>::iterator it = monitors_.begin() ; it != monitors_.end(); ++it) {
-    if((*it)->is_converged()){
+  for (auto& m : monitors_) {
+    if (m->is_converged()) {
       return true;
     }
   }
