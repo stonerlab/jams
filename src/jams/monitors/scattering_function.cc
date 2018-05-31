b93c87a929f02ba002258e970c4d62fb899750cc
Joseph Barker <joseph.barker@imr.tohoku.ac.jp>
2018-06-01 08:36:09 +0900

    remove commented out code

--- src/jams/monitors/scattering_function.cc
+++ src/jams/monitors/scattering_function.cc
@@ -34,20 +34,6 @@ void ScatteringFunctionMonitor::update(Solver *solver) {
   using namespace globals;
   using namespace std;
 
-//  std::vector<double> sxt(num_spins);
-//  std::vector<double> syt(num_spins);
-//  std::vector<double> szt(num_spins);
-//
-//  for (auto i = 0; i < num_spins; ++i) {
-//    sxt[i] = s(i, 0);
-//    syt[i] = s(i, 1);
-//    szt[i] = s(i, 2);
-//  }
-//
-//  sx_.push_back(sxt);
-//  sy_.push_back(syt);
-//  sz_.push_back(szt);
-
   const double kmax = 0.5;
   vector<complex<double>> fx_k(num_kpoints_, {0.0, 0.0});
   vector<complex<double>> fy_k(num_kpoints_, {0.0, 0.0});
@@ -62,9 +48,7 @@ void ScatteringFunctionMonitor::update(Solver *solver) {
 
     for (auto j = 0; j < num_spins; ++j) {
       const Vec3 r_j = lattice->atom_position(j);
-
       const Vec3 s_j = {s(j, 0), s(j, 1), s(j, 2)};
-
 
         fx_k[k] += s_j[0] * exp(-kImagTwoPi * dot(kz, r_j));
         fy_k[k] += s_j[1] * exp(-kImagTwoPi * dot(kz, r_j));
@@ -79,54 +63,6 @@ void ScatteringFunctionMonitor::update(Solver *solver) {
   fx_t.insert(fx_t.end(), fx_k.begin(), fx_k.end());
   fy_t.insert(fy_t.end(), fy_k.begin(), fy_k.end());
 
-//  const unsigned num_kpoints = 64;
-//  const double rmax = lattice->max_interaction_radius();
-//  const double kmax = 4;
-//
-//
-//  vector<double> f_k(num_kpoints, 0.0);
-//  vector<double> kvec(num_kpoints, 0.0);
-//  for (auto i = 0; i < num_kpoints; ++i) {
-//    kvec[i] = i * kmax/double(num_kpoints);
-//  }
-//
-//  for (auto i = 0; i < num_spins; ++i) {
-//    const Vec3 s_i = {s(i, 0), s(i, 1), s(i, 2)};
-//    const Vec3 r_i = lattice->atom_position(i);
-//
-//    for (auto j = 0; j < num_spins; ++j) {
-//      if (i == j) continue;
-//
-//      const Vec3 r_j = lattice->atom_position(j);
-//      const auto r_ij = lattice->displacement(r_i, r_j);
-//      const auto r = abs(r_ij);
-//
-//      if (r > rmax) continue;
-//
-//      const Vec3 s_j = {s(j, 0), s(j, 1), s(j, 2)};
-//
-//      auto xhat = normalize(r_ij);
-//      auto yhat = normalize(s_i - xhat * dot(s_i, xhat));
-//
-//      auto S_ix = dot(s_i, xhat);
-//      auto S_iy = dot(s_i, yhat);
-//
-//      auto S_jx = dot(s_j, xhat);
-//      auto S_jy = dot(s_j, yhat);
-//
-//      auto Sxx = S_ix * S_jx;
-//      auto Syy = S_iy * S_jy;
-//
-//      for (auto k = 0; k < num_kpoints; ++k) {
-//        f_k[k] += Syy * (sin(kvec[k] * r ) / r) + (2*Sxx - Syy) * ((sin(kvec[k] * r ) / pow3(kvec[k]*r) ) - (cos(kvec[k] * r ) / pow2(kvec[k] * r)));
-//      }
-//    }
-//  }
-//
-//  for (auto k = 0; k < num_kpoints; ++k) {
-//    outfile << solver->time() << " " << k << " " << kvec[k] << " " << fx_k[k].real() << " " << fy_k[k].real() << std::endl;
-//  }
-//  outfile << "\n" << std::endl;
 }
 
 bool ScatteringFunctionMonitor::is_converged() {
@@ -135,40 +71,6 @@ bool ScatteringFunctionMonitor::is_converged() {
 
 ScatteringFunctionMonitor::~ScatteringFunctionMonitor() {
   using namespace globals;
-//  std::ofstream freqfile("dsf.tsv");
-//
-//  double freq_delta  = 1.0/(num_samples_*t_sample_);
-//
-//  double kmax = 0.5;
-//  std::vector<double> kvec(num_kpoints_, 0.0);
-//  for (auto i = 0; i < num_kpoints_; ++i) {
-//    kvec[i] = i * kmax/double(num_kpoints_);
-//  }
-//
-//  unsigned num_wpoints_ = num_samples_;
-//
-//  for (auto kk = 0; kk < num_kpoints_; ++kk) {
-//    for (auto ww = 0; ww < num_samples_ / 2 + 1; ++ww) {
-//      std::complex<double> total = {0.0, 0.0};
-//      const Vec3 k = {0, 0, kvec[kk]};
-//      const double omega = ww * freq_delta;
-//      for (auto m = 0; m < num_samples_; ++m) {
-//      for (auto n = 0; n < 1; ++n) {
-//        for (auto i = 0; i < num_spins; ++i) {
-//          for (auto j = 0; j < num_spins; ++j) {
-//            const Vec3 r_i = lattice->atom_position(i);
-//            const Vec3 r_j = lattice->atom_position(j);
-//            double t = (m - n) * t_sample_;
-//            total += sx_[m][i] * sx_[num_samples_-1][j] * std::exp(kImagTwoPi * (dot(k, r_i - r_j) + omega * t));
-//          }
-//        }
-//      }
-//      }
-//      freqfile << kk << " " << omega << " " << total.real() << " " << total.imag() << std::endl;
-//    }
-//    freqfile << std::endl;
-//  }
-
 
   const int kpoints = num_kpoints_;
   const int time_points = static_cast<int>(fx_t.size() / kpoints);
@@ -179,8 +81,7 @@ ScatteringFunctionMonitor::~ScatteringFunctionMonitor() {
       fy_t[kpoints * i + j] *= fft_window_default(i, time_points);
     }
   }
-
-
+  
   auto fx_w = fx_t;
   auto fy_w = fy_t;
 
