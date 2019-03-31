934424df336a151b15caf173a7ac9f021335818a
Joseph Barker <j.barker@leeds.ac.uk>
2019-03-31 21:57:58 +0100

    Fixes OMP compiler arguments passed to cmake

--- cmake/Dependencies.cmake
+++ cmake/Dependencies.cmake
@@ -4,6 +4,9 @@ find_package(Threads QUIET)
 if(JAMS_BUILD_OMP)
     find_package(OpenMP REQUIRED)
 
+    # For macos we have to pass the flags to cmake, this line separates them correctly
+    separate_arguments(OpenMP_CXX_FLAGS UNIX_COMMAND "${OpenMP_CXX_FLAGS}")
+
     # For CMake < 3.9, we need to make the target ourselves
     if(NOT TARGET OpenMP::OpenMP_CXX)
         find_package(Threads REQUIRED)
@@ -13,7 +16,6 @@ if(JAMS_BUILD_OMP)
         # Only works if the same flag is passed to the linker; use CMake 3.9+ otherwise (Intel, AppleClang)
         set_property(TARGET OpenMP::OpenMP_CXX
                 PROPERTY INTERFACE_LINK_LIBRARIES ${OpenMP_CXX_FLAGS} Threads::Threads)
-
     endif()
 endif()
 
