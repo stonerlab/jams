94de04d771e43fbd2b7a4a50631dfecf2c8e6aff
Joseph Barker <j.barker@leeds.ac.uk>
2019-07-11 11:33:58 +0100

    Fix Cuda dipole tests

--- test/jams/jams_test.cc
+++ test/jams/jams_test.cc
@@ -1,8 +1,14 @@
 #include "gtest/gtest.h"
 #include "jams/containers/test_synced_memory.h"
 #include "jams/containers/test_multiarray.h"
+
+#ifdef HAS_CUDA
+#include "jams/hamiltonian/test_cuda_dipole_fft.h"
+#endif
 #include "jams/hamiltonian/test_dipole_cpu_tensor.h"
 #include "jams/hamiltonian/test_dipole_cpu_fft.h"
+
+
 
 
 int main(int argc, char **argv) {
