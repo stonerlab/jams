e83096ba5b283935b3fa8a4a8a6626ef63339426
Joseph Barker <j.barker@leeds.ac.uk>
2019-07-19 00:13:23 +0100

    Adds standard FFT for supercell data

--- src/jams/interface/fft.h
+++ src/jams/interface/fft.h
@@ -13,11 +13,12 @@
 #include <fftw3.h>
 #define __GNUC__ ORIG_GNUC
 
-
 #include <complex>
 #include <array>
 
 #include "jams/containers/multiarray.h"
+
+using Complex = std::complex<double>;
 
 #define FFTW_COMPLEX_CAST(x) reinterpret_cast<fftw_complex*>(x)
 
@@ -29,6 +30,36 @@ double fft_window_exponential(const int n, const int n_total);
 
 fftw_plan fft_plan_rspace_to_kspace(std::complex<double> * rspace, std::complex<double> * kspace, const std::array<int,3>& kspace_size, const int & motif_size);
 void apply_kspace_phase_factors(jams::MultiArray<std::complex<double>, 5> &kspace);
+
+void fft_supercell_vector_field_to_kspace(jams::MultiArray<double, 2>& rspace_data, jams::MultiArray<Vec3cx,4>& kspace_data, const Vec3i& kspace_size, const int & num_sites);
+void fft_supercell_scalar_field_to_kspace(jams::MultiArray<double, 1>& rspace_data, jams::MultiArray<Complex,4>& kspace_data, const Vec3i& kspace_size, const int & num_sites);
+
+template <std::size_t N>
+struct FFTWHermitianIndex {
+    Vec<int, N> offset;
+    bool conj;
+};
+
+// returns true if k corresponds to a (virtual) hermitian element
+template <std::size_t N>
+inline bool fftw_r2c_need_hermitian_symmetric_index(Vec<int, N> &k, const Vec<int, N> &n) {
+  return (k[N-1] % n[N-1] + n[N-1]) % n[N-1] >= n[N-1] / 2 + 1;
+}
+
+// Maps an index for an FFTW ordered array into the correct location and conjugation
+//
+// FFTW real to complex (r2c) transforms only give N/2+1 outputs in the last dimensions
+// as all other values can be calculated by Hermitian symmetry. This function maps
+// the N-dimensional array position of a set of general indicies (positive, negative, larger than
+// the fft_size) to the correct location and sets a bool as to whether the value must
+// be conjugated
+template <std::size_t N>
+inline FFTWHermitianIndex<N> fftw_r2c_index(Vec<int, N> k, const Vec<int, N> &n) {
+  if (fftw_r2c_need_hermitian_symmetric_index(k, n)) {
+    return FFTWHermitianIndex<N>{(-k % n + n) % n, true};
+  }
+  return FFTWHermitianIndex<N>{(k % n + n) % n, false};
+}
 
 #endif  // JAMS_MONITOR_FFT_H
 
