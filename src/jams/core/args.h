9a2c975e18725db422a2a4cdfdaf4f15c6240c69
Joseph Barker <j.barker@leeds.ac.uk>
2019-11-12 10:16:31 +0000

    Parse flag arguments for JAMS

---
+++ src/jams/core/args.h
@@ -0,0 +1,21 @@
+//
+// Created by Joseph Barker on 2019-11-12.
+//
+
+#ifndef JAMS_ARGS_H
+#define JAMS_ARGS_H
+
+#include <string>
+
+namespace jams {
+
+    struct ProgramArgs {
+        bool        setup_only        = false;
+        std::string config_file_path  = "";
+        std::string config_file_patch = "";
+    };
+
+    ProgramArgs parse_args(int argc, char **argv);
+}
+
+#endif //JAMS_ARGS_H
