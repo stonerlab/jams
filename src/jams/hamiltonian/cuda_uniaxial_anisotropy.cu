8f0394adfab356ece915cff9c8993fb066685b9b
Joseph Barker <j.barker@leeds.ac.uk>
2019-12-07 21:25:44 +0000

    Refactor uniaxial anisotropy
    The code is now simplified (one K1, K2, K3 order can be specified per Hamiltonian) but allows anisotropies to be set per material or per motif position with no requirement that an anisotropy must be specified for every type/site.

--- src/jams/hamiltonian/cuda_uniaxial_anisotropy.cu
+++ src/jams/hamiltonian/cuda_uniaxial_anisotropy.cu
@@ -12,8 +12,23 @@ CudaUniaxialHamiltonian::CudaUniaxialHamiltonian(const libconfig::Setting &setti
 {
 }
 
+double CudaUniaxialHamiltonian::calculate_total_energy() {
+  calculate_energies();
+  double e_total = 0.0;
+  for (auto i = 0; i < energy_.size(); ++i) {
+    e_total += energy_(i);
+  }
+  return e_total;
+}
+
+void CudaUniaxialHamiltonian::calculate_energies() {
+  cuda_uniaxial_energy_kernel<<<(globals::num_spins+dev_blocksize_-1)/dev_blocksize_, dev_blocksize_, 0, dev_stream_.get()>>>
+  (globals::num_spins, power_, magnitude_.device_data(), axis_.device_data(), globals::s.device_data(), energy_.device_data());
+  DEBUG_CHECK_CUDA_ASYNC_STATUS;
+}
+
 void CudaUniaxialHamiltonian::calculate_fields() {
   cuda_uniaxial_field_kernel<<<(globals::num_spins+dev_blocksize_-1)/dev_blocksize_, dev_blocksize_, 0, dev_stream_.get()>>>
-            (globals::num_spins, num_coefficients_, power_.device_data(), magnitude_.device_data(), axis_.device_data(), globals::s.device_data(), field_.device_data());
+            (globals::num_spins, power_, magnitude_.device_data(), axis_.device_data(), globals::s.device_data(), field_.device_data());
   DEBUG_CHECK_CUDA_ASYNC_STATUS;
 }
