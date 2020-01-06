969c91c37170dce0a4ad2a3b5ea47c4609912b95
Sean <ll14s26s@leeds.ac.uk>
2020-01-06 09:55:33 +0000

    Update variable names to prevent then being defined twice

--- src/jams/hamiltonian/cubic_anisotropy.cc
+++ src/jams/hamiltonian/cubic_anisotropy.cc
@@ -27,7 +27,7 @@ using std::runtime_error;
 //          Can add in a check that two are orthogonal similar to the mumax implementation
 // }
 
-struct AnisotropySetting {
+struct AnisotropySetting_cube {
     unsigned power;
     double   energy;
     Vec3     axis1;
@@ -36,7 +36,7 @@ struct AnisotropySetting {
 };
 
 unsigned anisotropy_power_from_name_cube(const string name) {
-    if (name == "K1") return 1;
+    if (name == "K1") return 1; // This is outputted correctly
     if (name == "K2") return 2;
     throw runtime_error("Unsupported anisotropy: " + name);
 }
@@ -46,25 +46,25 @@ AnisotropySetting read_anisotropy_setting_cube(Setting &setting) {
         Vec3 axis1 = {setting[1][0], setting[1][1], setting[1][2]};
         Vec3 axis2 = {setting[2][0], setting[2][1], setting[2][2]};
         Vec3 axis3 = {setting[3][0], setting[3][1], setting[3][2]};
-        return AnisotropySetting{anisotropy_power_from_name_cube(setting.getParent().getName()), setting[0], normalize(axis1), normalize(axis2), normalize(axis3)};
+        return AnisotropySetting_cube{anisotropy_power_from_name_cube(setting.getParent().getName()), setting[0], normalize(axis1), normalize(axis2), normalize(axis3)};
     }
     if (setting.isScalar()) {
-        return AnisotropySetting{anisotropy_power_from_name_cube(setting.getParent().getName()), setting, {1.0, 0.0, 0.0}, {0.0, 1.0, 0.0}, {0.0, 0.0, 1.0}};
+        return AnisotropySetting_cube{anisotropy_power_from_name_cube(setting.getParent().getName()), setting, {1.0, 0.0, 0.0}, {0.0, 1.0, 0.0}, {0.0, 0.0, 1.0}};
     }
     throw runtime_error("Incorrectly formatted anisotropy setting");
 }
 
-vector<vector<AnisotropySetting>> read_all_anisotropy_settings_cube(const Setting &settings) {
-    vector<vector<AnisotropySetting>> anisotropies(lattice->num_materials());
-    auto anisotropy_names = {"K1", "K2"};
-    for (const auto name : anisotropy_names) {
+vector<vector<AnisotropySetting_cube>> read_all_anisotropy_settings_cube(const Setting &settings) {
+    vector<vector<AnisotropySetting_cube>> anisotropies_cube(lattice->num_materials());
+    auto anisotropy_names_cube = {"K1", "K2"};
+    for (const auto name : anisotropy_names_cube) {
         if (settings.exists(name)) {
             if (settings[name].getLength() != lattice->num_materials()) {
                 throw runtime_error("CubicHamiltonian: " + string(name) + "  must be specified for every material");
             }
 
             for (auto i = 0; i < settings[name].getLength(); ++i) {
-                anisotropies[i].push_back(read_anisotropy_setting_cube(settings[name][i]));
+                anisotropies_cube[i].push_back(read_anisotropy_setting_cube(settings[name][i]));
             }
         }
     }
@@ -81,45 +81,45 @@ CubicHamiltonian::CubicHamiltonian(const Setting &settings, const unsigned int n
     //            "CubicHamiltonian: anisotropy should only be specified in terms of K1, K2, K3 maybe you want UniaxialCoefficientHamiltonian?");
     //}
 
-    auto anisotropies = read_all_anisotropy_settings_cube(settings);
+    auto anisotropies_cube = read_all_anisotropy_settings_cube(settings);
 
     for (auto type = 0; type < lattice->num_materials(); ++type) {
         std::cout << "  " << lattice->material_name(type) << ":\n";
-        for (const auto& ani : anisotropies[type]) {
+        for (const auto& ani : anisotropies_cube[type]) {
             std::cout << "    " << ani.axis1 << "  " << ani.axis2 << "  " << ani.axis3 << "  " << ani.power << "  " << ani.energy << "\n";
         }
     }
 
-    num_coefficients_ = anisotropies[0].size();
+    num_coefficients_ = anisotropies_cube[0].size();
 
-    power_.resize(num_spins, anisotropies[0].size());
-    axis_.resize(num_spins, anisotropies[0].size(), 3, 3); //Added in another dimension of size 3
-    magnitude_.resize(num_spins, anisotropies[0].size());
+    power_.resize(num_spins, anisotropies_cube[0].size());
+    axis_.resize(num_spins, anisotropies_cube[0].size(), 3, 3); //Added in another dimension of size 3
+    magnitude_.resize(num_spins, anisotropies_cube[0].size());
 
     for (int i = 0; i < globals::num_spins; ++i) {
         auto type = lattice->atom_material_id(i);
-        for (auto j = 0; j < anisotropies[type].size(); ++j) {
-            power_(i, j) = anisotropies[type][j].power;
-            magnitude_(i, j) = anisotropies[type][j].energy * input_unit_conversion_;
+        for (auto j = 0; j < anisotropies_cube[type].size(); ++j) {
+            power_(i, j) = anisotropies_cube[type][j].power;
+            magnitude_(i, j) = anisotropies_cube[type][j].energy * input_unit_conversion_;
             for (auto k : {0,1,2}) {
-                axis_(i, j, k, 0) = anisotropies[type][j].axis1[k];
-                axis_(i, j, k, 1) = anisotropies[type][j].axis2[k];
-                axis_(i, j, k, 2) = anisotropies[type][j].axis3[k];
+                axis_(i, j, k, 0) = anisotropies_cube[type][j].axis1[k];
+                axis_(i, j, k, 1) = anisotropies_cube[type][j].axis2[k];
+                axis_(i, j, k, 2) = anisotropies_cube[type][j].axis3[k];
             }
         }
     }
 }
 
 
-double CubicHamiltonian::calculate_total_energy() {
+double CubicHamiltonian::calculate_total_energy_cube() {
     double e_total = 0.0;
     for (int i = 0; i < energy_.size(); ++i) {
-        e_total += calculate_one_spin_energy(i);
+        e_total += calculate_one_spin_energy_cube(i);
     }
     return e_total;
 }
 
-double CubicHamiltonian::calculate_one_spin_energy(const int i) {
+double CubicHamiltonian::calculate_one_spin_energy_cube(const int i) {
     using namespace globals;
     double energy = 0.0;
 
@@ -142,7 +142,7 @@ double CubicHamiltonian::calculate_one_spin_energy(const int i) {
     return energy;
 }
 
-double CubicHamiltonian::calculate_one_spin_energy_difference(const int i, const Vec3 &spin_initial,
+double CubicHamiltonian::calculate_one_spin_energy_difference_cube(const int i, const Vec3 &spin_initial,
                                                               const Vec3 &spin_final) {
     double e_initial = 0.0;
     double e_final = 0.0;
@@ -176,13 +176,13 @@ double CubicHamiltonian::calculate_one_spin_energy_difference(const int i, const
     return e_final - e_initial;
 }
 
-void CubicHamiltonian::calculate_energies() {
+void CubicHamiltonian::calculate_energies_cube() {
     for (int i = 0; i < energy_.size(); ++i) {
-        energy_(i) = calculate_one_spin_energy(i);
+        energy_(i) = calculate_one_spin_energy_cube(i);
     }
 }
 
-void CubicHamiltonian::calculate_one_spin_field(const int i, double local_field[3]) {
+void CubicHamiltonian::calculate_one_spin_field_cube(const int i, double local_field[3]) {
     using namespace globals;
     local_field[0] = 0.0;
     local_field[1] = 0.0;
@@ -225,7 +225,7 @@ void CubicHamiltonian::calculate_one_spin_field(const int i, double local_field[
     }*/
 }
 
-void CubicHamiltonian::calculate_fields() {
+void CubicHamiltonian::calculate_fields_cube() {
     using namespace globals;
     field_.zero();
 
