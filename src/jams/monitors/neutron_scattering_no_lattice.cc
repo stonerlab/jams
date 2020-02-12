36f7a2114071028b9600d4b778e97e6cc59c6d60
Mai Kameda <mai@imr.tohoku.ac.jp>
2020-02-12 19:41:53 +0000

    Some more comments are added.

--- src/jams/monitors/neutron_scattering_no_lattice.cc
+++ src/jams/monitors/neutron_scattering_no_lattice.cc
@@ -121,6 +121,8 @@ NeutronScatteringNoLatticeMonitor::calculate_unpolarized_cross_section(const jam
         }
           for (auto i : {0, 1, 2}) {
             for (auto j : {0, 1, 2}) {
+              //(*) We may have to remove dirac_delta(f) since this term should be subtracted all the time?
+              //(*) even when f is not equals to 0 (I think elastic terms are always taken into consideration)
               cross_section(f, k) += ff * (kronecker_delta(i, j) - Q[i] * Q[j]) * ((s_a[i] * s_b[j]) - dirac_delta(f) * s0[i] * s0[j]);
             }
           }
@@ -212,6 +214,8 @@ void NeutronScatteringNoLatticeMonitor::shift_periodogram_overlap() {
 void NeutronScatteringNoLatticeMonitor::output_static_structure_factor() {
   const auto num_time_points = kspace_spins_timeseries_.size(0);
 
+  //(*) I think we should take the thermal average using python after running the program.
+  //(*) In that case, don't we want to save files with different name for each periodgram (to_string(0-total_periods))?
   //static_structure_factor=spin-spin correlation at the same time
   ofstream ofs(seedname + "_static_structure_factor_path_" + to_string(0) + ".tsv");
 
@@ -251,6 +255,10 @@ void NeutronScatteringNoLatticeMonitor::output_neutron_cross_section() {
 
     // sample time is here because the fourier transform in time is not an integral
     // but a discrete sum
+    //(*) Don't we have to remove (periodogram_props_.sample_time/kTwoPi) cause there shuld be
+    // another "1/periodogram_props_.sample_time" which comes from Dirac's delta function (Fermi's golden rule)?
+    //(*) Also we have to multiply kGyromagneticRatio to the prefactor?
+    //(*) Also we have to remove (0.5 * kNeutronGFactor) since it is already considered in the form factor?
     auto prefactor = (periodogram_props_.sample_time / double(total_periods_)) * (1.0 / (kTwoPi * kHBar))
                      * pow2((0.5 * kNeutronGFactor * pow2(kElementaryCharge)) / (kElectronMass * pow2(kSpeedOfLight)));
     auto barns_unitcell = prefactor / (1e-28);
