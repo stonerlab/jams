b15f9c0ee5ef90dbb92645aac8f573e14d98db74
Joseph Barker <j.barker@leeds.ac.uk>
2020-04-26 13:36:30 +0900

    Minor optimisation on some systems

--- src/jams/hamiltonian/dipole_neartree.cc
+++ src/jams/hamiltonian/dipole_neartree.cc
@@ -80,7 +80,7 @@ Vec3 DipoleNearTreeHamiltonian::calculate_one_spin_field(const int i)
     const Vec3 s_j = {s(j,0), s(j,1), s(j,2)};
     const Vec3 r_ij =  neighbour.first - r_i;
 
-    field += w0 * mus(j) * (3.0 * r_ij * dot(s_j, r_ij) - pow2(norm(r_ij)) * s_j) / pow5(norm(r_ij));
+    field += w0 * mus(j) * (3.0 * r_ij * dot(s_j, r_ij) - norm_sq(r_ij) * s_j) / pow5(norm(r_ij));
   }
   return field;
 }
