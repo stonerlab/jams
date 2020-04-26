223d3b01ccde0c87737b11699c6c1488464faafc
Joseph Barker <j.barker@leeds.ac.uk>
2020-04-26 10:23:56 +0900

    Re-enable some tests

--- src/jams/test/jams_test.cc
+++ src/jams/test/jams_test.cc
@@ -5,7 +5,7 @@
 #include "jams/test/containers/test_unordered_vector_set.h"
 #include "jams/test/containers/test_neartree.h"
 //#include "jams/test/containers/test_synced_memory.h"
-//#include "jams/test/containers/test_multiarray.h"
+#include "jams/test/containers/test_multiarray.h"
 
 #ifdef HAS_CUDA
 #include "jams/hamiltonian/test_cuda_dipole_fft.h"
