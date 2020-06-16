4c67a2f6ceaef9405bedb3d020e62618f45da2ad
Mai Kameda <mai@imr.tohoku.ac.jp>
2020-06-16 21:20:06 +0900

    [Fixed-14] Added crystal limit calculation where central site = center of the system.

--- src/jams/hamiltonian/exchange_functional.cc
+++ src/jams/hamiltonian/exchange_functional.cc
@@ -75,7 +75,7 @@ ExchangeFunctionalHamiltonian::ExchangeFunctionalHamiltonian(const libconfig::Se
 
     Vec3i lattice_dimensions = ::lattice->get_lattice_dimensions();
     rspace_displacement_(i) = ::lattice->displacement({lattice_dimensions[0]*0.5,lattice_dimensions[1]*0.5,lattice_dimensions[2]*0.5}, lattice->atom_position(i));
-    rspace_displacement2_(i) = ::lattice->displacement(lattice->atom_position(i),{0,0,0});
+    rspace_displacement2_(i) = ::lattice->atom_position(i);
     if( norm(rspace_displacement_(i)) < lattice_dimensions[0]*0.02 ){ //0.002047
         central_site_indx = i;
         cout << "central_site index: i = " << central_site_indx << endl;
