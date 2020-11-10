d717a45fa253889bb818b98848b33bc607117fa3
Giannis <pyic@leeds.ac.uk>
2020-11-10 11:45:25 +0000

    Plain Metadynamics Function Added
    Added nd compiled the main member functions required to run  plain metadynamics

--- src/jams/solvers/cpu_metadynamics_metropolis_solver.cpp
+++ src/jams/solvers/cpu_metadynamics_metropolis_solver.cpp
@@ -1,5 +1,6 @@
+#include <climits>
 //
-// Created by ioannis charalampidis on 07/11/2020.
+// Created by Ioannis Charalampidis on 07/11/2020.
 //
 
 #include "cpu_metadynamics_metropolis_solver.h"
@@ -11,9 +12,19 @@
 #include <random>
 #include "jams/helpers/random.h"
 #include <vector>
+#include <assert.h>
 
 
 using namespace std;
+
+void MetadynamicsMetropolisSolver::run() {
+  intialise_potential_histograms(potential_1D,potential_2D,sample_points_1d,sample_points_2d,sample_points_m_perpendicular);
+  //to insert gaussian we need to know the magnetisation value at any given point of the simulation, perform the interpolation and use the function.
+  //Also need to be able to just calculate the potential difference between 2 given points.
+  // should we use flags to specify when we need to calculate the energy difference and when to add a gausian to our histogram ?!
+
+}
+
 std::vector<double> MetadynamicsMetropolisSolver::linear_space(const double &min,const double &max,const double &step) {
   assert(min < max);
   vector<double> space;
@@ -25,3 +36,60 @@ std::vector<double> MetadynamicsMetropolisSolver::linear_space(const double &min
 
   return space;
 }
+
+double MetadynamicsMetropolisSolver::gaussian(const double &x,const double &center,const double &amplitude,const double &width) {
+  return amplitude*exp(- (x-center)*(x-center ) / (2.0 * width*width));
+}
+
+void MetadynamicsMetropolisSolver::intialise_potential_histograms(std::vector<double> &potential_1D, std::vector<std::vector<double>> &potential_2D,const std::vector<double> sample_points_1d,const std::vector<double> sample_points_2d, const std::vector<double> sample_points_m_perpendicular)  {
+
+  potential_1D.resize(sample_points_1d.size(),0.0);
+  potential_2D.resize(sample_points_2d.size(),sample_points_m_perpendicular);
+}
+
+void MetadynamicsMetropolisSolver::insert_gaussian(const double &center, const double &amplitude, const double &width, const vector<double> &sample_points, vector<double> &discrete_potential) {
+  assert(sample_points.size() == discrete_potential.size());
+  for (auto i = 0; i < sample_points.size(); ++i) {
+	discrete_potential[i] += gaussian(sample_points[i], center, amplitude, width);
+  }
+  // calculate the center position for a gaussian according to mirror boundary conditions
+  double mirrored_center;
+  if (center >=0) {
+	mirrored_center = 2 - center;
+  } else {
+	mirrored_center = -2 - center;
+  }
+  assert(mirrored_center >= -2 && mirrored_center <= 2);
+
+  // insert the mirrored gaussian
+  for (auto i = 0; i < sample_points.size(); ++i) {
+	discrete_potential[i] += gaussian(sample_points[i], mirrored_center, amplitude, width);
+  }
+}
+double MetadynamicsMetropolisSolver::interpolated_potential(const vector<double> &sample_points,const vector<double> &discrete_potential,const double &value)  {
+  assert(is_sorted(begin(sample_points), end(sample_points)));
+  assert(value > sample_points.front() || floats_are_equal(sample_points.front(), value));
+  assert(value < sample_points.back() || floats_are_equal(sample_points.back(), value));
+  assert(sample_points.size() == discrete_potential.size());
+  // TODO: Test if this gives the correct points for the interpolation
+  auto lower = floor((value - sample_points[0]) / (sample_points[1] - sample_points[0]));
+  auto upper = lower+1;
+  assert(lower < upper);
+  cout << "Indices Lower:" << lower <<endl;
+  return linear_interpolation(value, sample_points[lower], sample_points[upper],
+							  discrete_potential[lower], discrete_potential[upper]);
+}
+bool MetadynamicsMetropolisSolver::floats_are_equal(const double &x, const double &y, const double epsilon) {
+  return abs(x - y) < epsilon;
+}
+double MetadynamicsMetropolisSolver::linear_interpolation(const double &x,const double &x_lower,const double &x_upper,const double &y_lower,const double &y_upper) {
+  assert(x_lower < x_upper);
+  assert(x > x_lower || floats_are_equal(x, x_lower));
+  assert(x < x_upper || floats_are_equal(x, x_upper));
+  auto a =y_lower + (x - x_lower)*(y_upper - y_lower) / (x_upper - x_lower);
+
+  return y_lower + (x - x_lower)*(y_upper - y_lower) / (x_upper - x_lower);
+}
+
+
+
