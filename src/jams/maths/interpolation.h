3b664776928225369f58e128c552b762e657189c
Joseph Barker <j.barker@leeds.ac.uk>
2020-11-12 16:35:53 +0000

    Make interpolation function inline

--- src/jams/maths/interpolation.h
+++ src/jams/maths/interpolation.h
@@ -11,12 +11,12 @@ namespace jams {
     namespace maths {
         /// 1D linear interpolation to find y at point x given lower coordinate
         /// (x_lower, y_lower) and upper coordinate (x_upper, y_upper)
-        double linear_interpolation(const double &x,const double &x_lower, const double &y_lower, const double &x_upper, const double &y_upper) {
+        inline double linear_interpolation(const double &x,const double &x_lower, const double &y_lower, const double &x_upper, const double &y_upper) {
           assert(x_lower < x_upper);
           assert(x > x_lower || approximately_equal(x, x_lower));
           assert(x < x_upper || approximately_equal(x, x_upper));
 
-          return y_lower + (x - x_lower)*(y_upper - y_lower) / (x_upper - x_lower);
+          return y_lower + (x - x_lower) * (y_upper - y_lower) / (x_upper - x_lower);
         }
     }
 }
