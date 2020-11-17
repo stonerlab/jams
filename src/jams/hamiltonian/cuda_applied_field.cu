9daa2d4e9c32ecef544bd9844a37e438df3e9203
Joseph Barker <j.barker@leeds.ac.uk>
2020-11-17 16:56:37 +0000

    Fix CudaAppliedField
    We cannot copy a double[3] to a kernel and should use a double3 instead.

--- src/jams/hamiltonian/cuda_applied_field.cu
+++ src/jams/hamiltonian/cuda_applied_field.cu
@@ -7,16 +7,15 @@ CudaAppliedFieldHamiltonian::CudaAppliedFieldHamiltonian(
 
 void CudaAppliedFieldHamiltonian::calculate_fields() {
   dim3 block_size;
-  block_size.x = 32;
-  block_size.y = 4;
+  block_size.x = 64;
 
   dim3 grid_size;
   grid_size.x = (globals::num_spins + block_size.x - 1) / block_size.x;
-  grid_size.y = (3 + block_size.y - 1) / block_size.y;
 
+  const double3 field = {b_field()[0], b_field()[1], b_field()[2]};
   cuda_applied_field_kernel<<<grid_size, block_size, 0, cuda_stream_.get() >>>
         (globals::num_spins, globals::mus.device_data(),
-        b_field().data(), field_.device_data());
+         field, field_.device_data());
     DEBUG_CHECK_CUDA_ASYNC_STATUS;
 }
 
@@ -27,8 +26,9 @@ void CudaAppliedFieldHamiltonian::calculate_energies() {
   dim3 grid_size;
   grid_size.x = (globals::num_spins + block_size.x - 1) / block_size.x;
 
+  const double3 field = {b_field()[0], b_field()[1], b_field()[2]};
   cuda_applied_field_energy_kernel<<<grid_size, block_size, 0, cuda_stream_.get() >>>
       (globals::num_spins, globals::s.device_data(), globals::mus.device_data(),
-       b_field().data(), energy_.device_data());
+       field, energy_.device_data());
   DEBUG_CHECK_CUDA_ASYNC_STATUS;
 }
--- src/jams/hamiltonian/cuda_applied_field_kernel.cuh
+++ src/jams/hamiltonian/cuda_applied_field_kernel.cuh
@@ -3,21 +3,21 @@
 
 #include <array>
 
-__global__ void cuda_applied_field_energy_kernel(const unsigned int num_spins, const double * dev_s, const double * dev_mus, const double b_field[3], double * dev_e) {
+__global__ void cuda_applied_field_energy_kernel(const unsigned int num_spins, const double * dev_s, const double * dev_mus, const double3 b_field, double * dev_e) {
   const unsigned int idx = blockIdx.x * blockDim.x + threadIdx.x;
 
   if (idx < num_spins) {
-    dev_e[idx] = -dev_mus[idx] * (dev_s[3 * idx + 0] * b_field[0] + dev_s[3 * idx + 1] * b_field[1] + dev_s[3 * idx + 2] * b_field[2]);
+    dev_e[idx] = -dev_mus[idx] * (dev_s[3 * idx + 0] * b_field.x + dev_s[3 * idx + 1] * b_field.y + dev_s[3 * idx + 2] * b_field.z);
   }
 }
 
-__global__ void cuda_applied_field_kernel(const unsigned int num_spins, const double * dev_mus, const double b_field[3], double * dev_h) {
+__global__ void cuda_applied_field_kernel(const unsigned int num_spins, const double * dev_mus, const double3 b_field, double * dev_h) {
   const unsigned int idx = blockIdx.x * blockDim.x + threadIdx.x;
-  const unsigned int idy = blockIdx.y * blockDim.y + threadIdx.y;
-  const unsigned int gxy = 3 * idx + idy;
 
-  if (idx < num_spins && idy < 3) {
-    dev_h[gxy] += dev_mus[idx] * b_field[idy];
+  if (idx < num_spins) {
+    dev_h[3*idx + 0] = dev_mus[idx] * b_field.x;
+    dev_h[3*idx + 1] = dev_mus[idx] * b_field.y;
+    dev_h[3*idx + 2] = dev_mus[idx] * b_field.z;
   }
 }
 
