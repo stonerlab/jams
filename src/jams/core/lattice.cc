8bdf3579aa94e5c39f6d3135330b3d50b299e427
Sean <ll14s26s@leeds.ac.uk>
2020-12-02 13:20:54 +0000

    Normalise spin vector upon reading config

--- src/jams/core/lattice.cc
+++ src/jams/core/lattice.cc
@@ -708,10 +708,10 @@ void Lattice::generate_supercell(const libconfig::Setting &lattice_settings)
       globals::s(i, n) = material.spin[n];
     }
 
-    double s_len = sqrt(globals::s(i,0)*globals::s(i,0) + globals::s(i,1)*globals::s(i,1) + globals::s(i,2)*globals::s(i,2))
+    double s_len = sqrt(globals::s(i,0)*globals::s(i,0) + globals::s(i,1)*globals::s(i,1) + globals::s(i,2)*globals::s(i,2));
 
     for (auto n = 0; n < 3; ++n) {
-        globals::s(i, n) = global::s(i, n)/s_len;
+        globals::s(i, n) = globals::s(i, n)/s_len;
     }
 
     if (material.randomize) {
