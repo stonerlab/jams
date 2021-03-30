33b9d8de5d60b8bda4797bbe4548e9ca3bd6411f
Joseph Barker <53377004+drjbarker@users.noreply.github.com>
2021-03-30 17:29:33 +0100

    Enable github actions on repository (#106)
    Adds github action support to use self hosted runner on the development server. Standard pushes will do a quick build check while any push or pull request onto the master branch will do more comprehensive builds.

--- cmake/External/highfive.cmake
+++ cmake/External/highfive.cmake
@@ -1,30 +1,40 @@
 include(${PROJECT_SOURCE_DIR}/cmake/Modules/DownloadProject.cmake)
 
-set(USE_BOOST OFF CACHE INTERNAL "")
-set(HIGHFIVE_EXAMPLES OFF CACHE INTERNAL "")
-set(HIGHFIVE_UNIT_TESTS OFF CACHE INTERNAL "")
+if (DEFINED JAMS_HIGHFIVE_VERSION)
+    set(USE_BOOST OFF CACHE INTERNAL "")
+    set(HIGHFIVE_EXAMPLES OFF CACHE INTERNAL "")
+    set(HIGHFIVE_UNIT_TESTS OFF CACHE INTERNAL "")
 
-set(PROJ_CMAKE_ARGS -DUSE_BOOST=OFF -DHIGHFIVE_EXAMPLES=OFF -DHIGHFIVE_UNIT_TESTS=OFF)
+    set(PROJ_CMAKE_ARGS -DUSE_BOOST=OFF -DHIGHFIVE_EXAMPLES=OFF -DHIGHFIVE_UNIT_TESTS=OFF)
 
-set(JAMS_HIGHFIVE_URL "https://github.com/BlueBrain/HighFive.git")
-if (MESSAGE_QUIET AND (NOT DEFINED VERBOSE))
-    download_project(
-            PROJ                HighFive
-            GIT_REPOSITORY      ${JAMS_HIGHFIVE_URL}
-            GIT_TAG             ${JAMS_HIGHFIVE_VERSION}
-            CMAKE_ARGS          ${PROJ_CMAKE_ARGS}
-            QUIET
-    )
+    set(JAMS_HIGHFIVE_URL "https://github.com/BlueBrain/HighFive.git")
+    if (MESSAGE_QUIET AND (NOT DEFINED VERBOSE))
+        download_project(
+                PROJ                HighFive
+                GIT_REPOSITORY      ${JAMS_HIGHFIVE_URL}
+                GIT_TAG             ${JAMS_HIGHFIVE_VERSION}
+                GIT_SHALLOW         ON
+                CMAKE_ARGS          ${PROJ_CMAKE_ARGS}
+                QUIET
+        )
+    else()
+        download_project(
+                PROJ                HighFive
+                GIT_REPOSITORY      ${JAMS_HIGHFIVE_URL}
+                GIT_TAG             ${JAMS_HIGHFIVE_VERSION}
+                GIT_SHALLOW         ON
+                CMAKE_ARGS          ${PROJ_CMAKE_ARGS}
+        )
+    endif()
+
+    add_subdirectory(${HighFive_SOURCE_DIR} ${HighFive_BINARY_DIR} EXCLUDE_FROM_ALL)
+
+    add_library(highfive ALIAS HighFive)
+    set(JAMS_HIGHFIVE_LIBRARIES "built-in (git)")
 else()
-    download_project(
-            PROJ                HighFive
-            GIT_REPOSITORY      ${JAMS_HIGHFIVE_URL}
-            GIT_TAG             ${JAMS_HIGHFIVE_VERSION}
-            CMAKE_ARGS          ${PROJ_CMAKE_ARGS}
-    )
+    find_package(HighFive REQUIRED)
+
+    add_library(highfive INTERFACE)
+    target_link_libraries(highfive INTERFACE HighFive)
+    set(JAMS_HIGHFIVE_LIBRARIES ${HighFive_FOUND})
 endif()
-
-add_subdirectory(${HighFive_SOURCE_DIR} ${HighFive_BINARY_DIR} EXCLUDE_FROM_ALL)
-
-add_library(highfive_builtin ALIAS HighFive)
-set(JAMS_HIGHFIVE_LIBRARIES "built-in (git)")
--- cmake/External/spglib.cmake
+++ cmake/External/spglib.cmake
@@ -1,24 +1,34 @@
 include(${PROJECT_SOURCE_DIR}/cmake/Modules/DownloadProject.cmake)
 
-set(JAMS_SPGLIB_URL "https://github.com/spglib/spglib.git")
-if (MESSAGE_QUIET AND (NOT DEFINED VERBOSE))
-    download_project(
-            PROJ                spglib
-            GIT_REPOSITORY      ${JAMS_SPGLIB_URL}
-            GIT_TAG             ${JAMS_SPGLIB_VERSION}
-            QUIET
-    )
+if (DEFINED JAMS_SPGLIB_VERSION)
+    set(JAMS_SPGLIB_URL "https://github.com/spglib/spglib.git")
+    if (MESSAGE_QUIET AND (NOT DEFINED VERBOSE))
+        download_project(
+                PROJ                spglib
+                GIT_REPOSITORY      ${JAMS_SPGLIB_URL}
+                GIT_TAG             ${JAMS_SPGLIB_VERSION}
+                GIT_SHALLOW         ON
+                QUIET
+        )
+    else()
+        download_project(
+                PROJ                spglib
+                GIT_REPOSITORY      ${JAMS_SPGLIB_URL}
+                GIT_TAG             ${JAMS_SPGLIB_VERSION}
+                GIT_SHALLOW         ON
+        )
+    endif()
+
+    add_subdirectory(${spglib_SOURCE_DIR} ${spglib_BINARY_DIR} EXCLUDE_FROM_ALL)
+    target_include_directories(symspg PUBLIC $<BUILD_INTERFACE:${spglib_SOURCE_DIR}/src>)
+    target_include_directories(symspg_static PUBLIC $<BUILD_INTERFACE:${spglib_SOURCE_DIR}/src>)
+
+    add_library(spglib ALIAS symspg_static)
+    set(JAMS_SPGLIB_LIBRARIES "built-in (git)")
 else()
-    download_project(
-            PROJ                spglib
-            GIT_REPOSITORY      ${JAMS_SPGLIB_URL}
-            GIT_TAG             ${JAMS_SPGLIB_VERSION}
-    )
+    find_package(SYMSPG REQUIRED)
+    add_library(spglib INTERFACE)
+    target_link_libraries(spglib INTERFACE ${SYMSPG_LIBRARY})
+    target_include_directories(spglib INTERFACE ${SYMSPG_INCLUDE_DIR})
+    set(JAMS_SPGLIB_LIBRARIES ${SYMSPG_LIBRARY})
 endif()
-
-add_subdirectory(${spglib_SOURCE_DIR} ${spglib_BINARY_DIR} EXCLUDE_FROM_ALL)
-target_include_directories(symspg PUBLIC $<BUILD_INTERFACE:${spglib_SOURCE_DIR}/src>)
-target_include_directories(symspg_static PUBLIC $<BUILD_INTERFACE:${spglib_SOURCE_DIR}/src>)
-
-add_library(spglib_builtin ALIAS symspg_static)
-set(JAMS_SPGLIB_LIBRARIES "built-in (git)")
