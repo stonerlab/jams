e091b859e59ba614784d7ccd08abf096388e2835
Joseph Barker <j.barker@leeds.ac.uk>
2021-08-11 11:42:40 +0100

    Bump HighFive to v2.3.1

--- src/jams/interface/highfive.h
+++ src/jams/interface/highfive.h
@@ -13,43 +13,49 @@
 namespace HighFive {
     namespace details {
 
-        template<class Tp_, std::size_t Dim_, class Idx_>
-        struct type_of_array<jams::MultiArray<Tp_, Dim_, Idx_>> {
-        typedef typename type_of_array<Tp_>::type type;
-    };
-
     template<class Tp_, std::size_t Dim_, class Idx_>
-    struct array_dims<jams::MultiArray<Tp_, Dim_, Idx_>> {
-    static constexpr size_t value = Dim_;
-};
+    struct inspector<jams::MultiArray<Tp_, Dim_, Idx_>> {
+        using type = jams::MultiArray<Tp_, Dim_, Idx_>;
+        using value_type = Tp_;
+        using base_type = typename inspector<value_type>::base_type;
+
+        static constexpr size_t ndim = Dim_;
+        static constexpr size_t recursive_ndim = ndim + inspector<value_type>::recursive_ndim;
+
+        static std::array<size_t, recursive_ndim> getDimensions(const type& val) {
+          std::array<size_t, recursive_ndim> sizes{val.size()};
+          size_t index = ndim;
+          for (const auto& s: inspector<value_type>::getDimensions(val[0])) {
+            sizes[index++] = s;
+          }
+          return sizes;
+        }
+    };
 
 // apply conversion to jams::MultiArray
 template<class Tp_, std::size_t Dim_, class Idx_>
-struct data_converter<jams::MultiArray<Tp_, Dim_, Idx_>, void> {
+struct data_converter<jams::MultiArray<Tp_, Dim_, Idx_>, void>
+    : public container_converter<jams::MultiArray<Tp_, Dim_, Idx_>> {
 
-typedef typename jams::MultiArray<Tp_, Dim_, Idx_> MultiArray;
 
-inline data_converter(MultiArray&, DataSpace& space)
-    : _dims(space.getDimensions()) {
-  assert(_dims.size() == Dim_);
-}
+    using MultiArray = jams::MultiArray<Tp_, Dim_, Idx_>;
+    using value_type = typename inspector<Tp_>::base_type;
+    using container_converter<MultiArray>::container_converter;
 
-inline typename type_of_array<Tp_>::type* transform_read(MultiArray& array) {
-  if (std::equal(_dims.begin(), _dims.end(), std::begin(array.shape())) == false) {
-    std::array<Idx_, Dim_> ext;
-    std::copy(_dims.begin(), _dims.end(), ext.begin());
-    array.resize(ext);
+  inline value_type* transform_read(MultiArray& array) {
+    if (std::equal(_dims.begin(), _dims.end(), std::begin(array.shape())) == false) {
+      std::array<Idx_, Dim_> ext;
+      std::copy(_dims.begin(), _dims.end(), ext.begin());
+      array.resize(ext);
+    }
+    return array.data();
   }
-  return array.data();
-}
 
-inline typename type_of_array<Tp_>::type* transform_write(MultiArray& array) {
-  return array.data();
-}
+  inline const value_type* transform_write(const MultiArray& array) const noexcept{
+    return array.data();
+  }
 
-inline void process_result(MultiArray&) {}
-
-std::vector<size_t> _dims;
+  std::vector<size_t> _dims;
 };
 }
 }
