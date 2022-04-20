67a113027bfe3bcf2bdb17b9bee2a3dc6a2ca183
Joseph Barker <53377004+drjbarker@users.noreply.github.com>
2022-04-20 13:46:52 +0100

    Add new MKL sparse matrix interface (#133)
    * Add new MKL sparse matrix interface
    
    The NIST style sparse BLAS API was deprecated in MKL >= 2018. I've now added the new 'Inspector Executor' API alongside the old API. The new API is used when compiled with MKL >= 2018.

--- src/jams/containers/sparse_matrix.h
+++ src/jams/containers/sparse_matrix.h
@@ -19,6 +19,16 @@
 
 #if HAS_MKL
 #include <mkl_spblas.h>
+#include <mkl_version.h>
+#endif
+
+// Since MKL >= 2018 the NIST sparse BLAS interface is deprecated in MKL and
+// replaced with the new 'Inspector Executor' API. This macro is '1' if
+// the Inspector Executor API is available.
+#if defined(HAS_MKL) && INTEL_MKL_VERSION >= 20180000
+#define HAS_MKL_INSPECTOR_EXECUTOR_API 1
+#else
+#define HAS_MKL_INSPECTOR_EXECUTOR_API 0
 #endif
 
 #ifdef HAS_CUDA
@@ -61,7 +71,7 @@ namespace jams {
         inline SparseMatrix(const index_type num_rows, const index_type num_cols, const index_type num_non_zero,
                             index_container rows, index_container cols, value_container vals,
                             SparseMatrixFormat format, SparseMatrixType type, SparseMatrixFillMode fill_mode)
-            : description_(format, type, fill_mode, SparseMatrixDiagType::NON_UNIT),
+            : matrix_A_description_(format, type, fill_mode, SparseMatrixDiagType::NON_UNIT),
               num_rows_(num_rows),
               num_cols_(num_cols),
               num_non_zero_(num_non_zero),
@@ -71,20 +81,27 @@ namespace jams {
         }
 
         inline ~SparseMatrix() {
+          #if HAS_MKL_INSPECTOR_EXECUTOR_API
+          if (mkl_matrix_A_handle_) {
+            mkl_sparse_destroy(mkl_matrix_A_handle_);
+            mkl_matrix_A_handle_ = nullptr;
+          }
+          #endif
+
           #if HAS_CUSPARSE_GENERIC_API
-          if (cusparse_sp_mat_descr_) {
-            cusparseDestroySpMat(cusparse_sp_mat_descr_);
-            cusparse_sp_mat_descr_ = nullptr;
+          if (cusparse_matrix_A_handle_) {
+            cusparseDestroySpMat(cusparse_matrix_A_handle_);
+            cusparse_matrix_A_handle_ = nullptr;
           }
 
-          if (vector_dn_vec_descr_) {
-            cusparseDestroyDnVec(vector_dn_vec_descr_);
-            vector_dn_vec_descr_ = nullptr;
+          if (cusparse_vector_x_handle_) {
+            cusparseDestroyDnVec(cusparse_vector_x_handle_);
+            cusparse_vector_x_handle_ = nullptr;
           }
 
-          if (vector_dn_vec_descr_) {
-            cusparseDestroyDnVec(result_dn_vec_descr_);
-            result_dn_vec_descr_ = nullptr;
+          if (cusparse_vector_x_handle_) {
+            cusparseDestroyDnVec(cusparse_vector_y_handle_);
+            cusparse_vector_y_handle_ = nullptr;
           }
 
           if (cusparse_buffer_) {
@@ -95,11 +112,11 @@ namespace jams {
           #endif
         }
 
-        inline constexpr SparseMatrixFormat format() const { return description_.format(); }
+        inline constexpr SparseMatrixFormat format() const { return matrix_A_description_.format(); }
 
-        inline constexpr SparseMatrixType type() const { return description_.type(); }
+        inline constexpr SparseMatrixType type() const { return matrix_A_description_.type(); }
 
-        inline constexpr SparseMatrixFillMode fill_mode() const { return description_.fill_mode(); }
+        inline constexpr SparseMatrixFillMode fill_mode() const { return matrix_A_description_.fill_mode(); }
 
         inline constexpr index_type num_non_zero() const { return num_non_zero_; }
 
@@ -117,27 +134,39 @@ namespace jams {
 
         inline constexpr std::size_t memory() const { return row_.memory() + col_.memory() + val_.memory(); };
 
+        // Performs the multiplication y = A * x where 'A' is this sparse matrix
+        // and vectors x and y are dense vectors passed into the function.
         template<class U, size_t N>
-        void multiply(const MultiArray<U, N> &vector, MultiArray<U, N> &result) const;
+        void multiply(const MultiArray<U, N> &vector_x, MultiArray<U, N> &vector_y);
 
+
+        // Multiplies row 'i' of this sparse matrix by the dense vector x
+        // producing a single scalar.
         template<class U, size_t N>
-        U multiply_row(index_type i, const MultiArray<U, N> &vector) const;
+        U multiply_row(index_type i, const MultiArray<U, N> &vector_x) const;
 
         #if HAS_CUDA
+        // Performs the multiplication y = A * x where 'A' is this sparse matrix
+        // and vectors x and y are dense vectors passed into the function.
+        // Thus function performs the multiplication on the GPU.
         template <class U, size_t N>
-        void multiply_gpu(const MultiArray<U, N> &vector, MultiArray<U, N> &result,
+        void multiply_gpu(const MultiArray<U, N> &vector_x, MultiArray<U, N> &vector_y,
                           cusparseHandle_t handle, cudaStream_t stream_id);
         #endif
 
     protected:
-        SparseMatrixDescription description_;
+        SparseMatrixDescription matrix_A_description_;
+
+        #if HAS_MKL_INSPECTOR_EXECUTOR_API
+        sparse_matrix_t mkl_matrix_A_handle_ = nullptr;
+        #endif
 
         #if HAS_CUSPARSE_GENERIC_API
-        cusparseSpMatDescr_t cusparse_sp_mat_descr_ = nullptr;
+        cusparseSpMatDescr_t cusparse_matrix_A_handle_ = nullptr;
 
         // descriptors for dense vectors used in matrix multiplication
-        cusparseDnVecDescr_t vector_dn_vec_descr_ = nullptr;
-        cusparseDnVecDescr_t result_dn_vec_descr_ = nullptr;
+        cusparseDnVecDescr_t cusparse_vector_x_handle_ = nullptr;
+        cusparseDnVecDescr_t cusparse_vector_y_handle_ = nullptr;
 
         void*       cusparse_buffer_ = nullptr;
         std::size_t cusparse_buffer_size_ = 0;
@@ -153,21 +182,46 @@ namespace jams {
 
     template<typename T>
     template<class U, size_t N>
-    void SparseMatrix<T>::multiply(const MultiArray<U, N> &vector, MultiArray<U, N> &result) const {
-      switch (description_.format()) {
+    void SparseMatrix<T>::multiply(const MultiArray<U, N> &vector_x, MultiArray<U, N> &vector_y) {
+      switch (matrix_A_description_.format()) {
         case SparseMatrixFormat::COO:
           jams::Xcoomv_general(1.0, 0.0, num_rows_, num_non_zero_, val_.data(), col_.data(), row_.data(),
-              vector.data(), result.data());
+              vector_x.data(), vector_y.data());
           return;
         case SparseMatrixFormat::CSR:
           #if HAS_MKL
-          double one = 1.0, zero = 0.0;
-          const char transa[1] = {'N'};
-          mkl_dcsrmv(transa, &num_rows_, &num_cols_, &one, description_.mkl_desc(), val_.data(),
-                     col_.data(), row_.data(), row_.data() + 1, vector.data(), &zero, result.data());
+            // Since MKL >= 2018 the NIST sparse BLAS interface is being
+            // deprecated in MKL and replaced with the new 'Inspector Executor'
+            // API. This requires creating a sparse matrix handle which contains
+            // basic information about the matrix. We do this on the first
+            // call of this function and clean it up in the destructor.
+            //
+            // Because this matrix is constructed by a factory the internal
+            // structure of the matrix should not change so we don't need to
+            // keep checking if the handle needs updating.
+            //
+            // We retain the old interface for older versions of MKL.
+            #if HAS_MKL_INSPECTOR_EXECUTOR_API
+              if (!mkl_matrix_A_handle_) {
+                mkl_sparse_d_create_csr(&mkl_matrix_A_handle_,
+                                        SPARSE_INDEX_BASE_ZERO,
+                                        num_rows_, num_cols_,
+                                        row_.data(), row_.data() + 1,
+                                        col_.data(), val_.data());
+              }
+              mkl_sparse_d_mv(SPARSE_OPERATION_NON_TRANSPOSE,
+                              1.0, mkl_matrix_A_handle_,
+                              matrix_A_description_.mkl_inspector_executor_desc(),
+                              vector_x.data(), 0.0, vector_y.data());
+            #else
+              double one = 1.0, zero = 0.0;
+              const char transa[1] = {'N'};
+              mkl_dcsrmv(transa, &num_rows_, &num_cols_, &one, matrix_A_description_.mkl_desc(), val_.data(),
+                         col_.data(), row_.data(), row_.data() + 1, vector_x.data(), &zero, vector_y.data());
+            #endif
           #else
           jams::Xcsrmv_general(
-              1.0, 0.0, num_rows_, val_.data(), col_.data(), row_.data(), vector.data(), result.data());
+              1.0, 0.0, num_rows_, val_.data(), col_.data(), row_.data(), vector_x.data(), vector_y.data());
           #endif
           return;
       }
@@ -176,14 +230,14 @@ namespace jams {
 
     template<typename T>
     template<class U, size_t N>
-    U SparseMatrix<T>::multiply_row(const index_type i, const MultiArray<U, N> &vector) const {
-      switch (description_.format()) {
+    U SparseMatrix<T>::multiply_row(const index_type i, const MultiArray<U, N> &vector_x) const {
+      switch (matrix_A_description_.format()) {
         case SparseMatrixFormat::COO:
           return jams::Xcoomv_general_row(
-              val_.data(), col_.data(), row_.data(), vector.data(), i);
+              val_.data(), col_.data(), row_.data(), vector_x.data(), i);
         case SparseMatrixFormat::CSR:
           return jams::Xcsrmv_general_row(
-              val_.data(), col_.data(), row_.data(), vector.data(), i);
+              val_.data(), col_.data(), row_.data(), vector_x.data(), i);
       }
       throw std::runtime_error("Unknown sparse matrix format for SparseMatrix<T>::multiply_row");
     }
@@ -191,10 +245,10 @@ namespace jams {
     #ifdef HAS_CUDA
     template<typename T>
     template<class U, size_t N>
-    void SparseMatrix<T>::multiply_gpu(const MultiArray<U, N> &vector, MultiArray<U, N> &result,
+    void SparseMatrix<T>::multiply_gpu(const MultiArray<U, N> &vector_x, MultiArray<U, N> &vector_y,
                                        cusparseHandle_t handle, cudaStream_t stream_id) {
       assert(handle != nullptr);
-      switch (description_.format()) {
+      switch (matrix_A_description_.format()) {
         case SparseMatrixFormat::COO:
           throw std::runtime_error("unimplemented");
         case SparseMatrixFormat::CSR:
@@ -203,9 +257,9 @@ namespace jams {
 
     #if HAS_CUSPARSE_GENERIC_API
 
-          if (!cusparse_sp_mat_descr_) {
+          if (!cusparse_matrix_A_handle_) {
             CHECK_CUSPARSE_STATUS(cusparseCreateCsr(
-                &cusparse_sp_mat_descr_,
+                &cusparse_matrix_A_handle_,
                 num_rows_,
                 num_cols_,
                 num_non_zero_,
@@ -219,23 +273,23 @@ namespace jams {
             ));
           }
 
-          if (!vector_dn_vec_descr_) {
+          if (!cusparse_vector_x_handle_) {
             CHECK_CUSPARSE_STATUS(cusparseCreateDnVec(
-                &vector_dn_vec_descr_,
-                vector.elements(),
-                (void*)vector.device_data(),
+                &cusparse_vector_x_handle_,
+                vector_x.elements(),
+                (void*)vector_x.device_data(),
                 cuda::get_cuda_data_type<U>()));
           }
-          cusparseDnVecSetValues(vector_dn_vec_descr_, (void*)vector.device_data());
+          cusparseDnVecSetValues(cusparse_vector_x_handle_, (void*)vector_x.device_data());
 
-          if (!result_dn_vec_descr_) {
+          if (!cusparse_vector_y_handle_) {
             CHECK_CUSPARSE_STATUS(cusparseCreateDnVec(
-                &result_dn_vec_descr_,
-                result.elements(),
-                (void*)result.device_data(),
+                &cusparse_vector_y_handle_,
+                vector_y.elements(),
+                (void*)vector_y.device_data(),
                 cuda::get_cuda_data_type<U>()));
           }
-          cusparseDnVecSetValues(result_dn_vec_descr_, (void*)result.device_data());
+          cusparseDnVecSetValues(cusparse_vector_y_handle_, (void*)vector_y.device_data());
 
           size_t new_buffer_size = 0;
 
@@ -243,10 +297,10 @@ namespace jams {
               handle,
               CUSPARSE_OPERATION_NON_TRANSPOSE,
               &one,
-              cusparse_sp_mat_descr_,
-              vector_dn_vec_descr_,
+              cusparse_matrix_A_handle_,
+              cusparse_vector_x_handle_,
               &zero,
-              result_dn_vec_descr_,
+              cusparse_vector_y_handle_,
               // Note: the type selection here may be more complicated in general.
               // The compute type depends on the types of A/X/Y.
               // see https://docs.nvidia.com/cuda/cusparse/index.html#cusparse-generic-function-spmv
@@ -266,10 +320,10 @@ namespace jams {
               handle,
               CUSPARSE_OPERATION_NON_TRANSPOSE,
               &one,
-              cusparse_sp_mat_descr_,
-              vector_dn_vec_descr_,
+              cusparse_matrix_A_handle_,
+              cusparse_vector_x_handle_,
               &zero,
-              result_dn_vec_descr_,
+              cusparse_vector_y_handle_,
               // Note: the type selection here may be more complex in general.
               // The compute type depends on the types of A/X/Y.
               // see (https://docs.nvidia.com/cuda/cusparse/index.html#cusparse-generic-function-spmv)
@@ -282,11 +336,11 @@ namespace jams {
              CUSPARSE_OPERATION_NON_TRANSPOSE,
              num_rows_, num_cols_, num_non_zero_,
              &one,
-             description_.cusparse_desc(),
+             matrix_A_description_.cusparse_desc(),
              val_.device_data(), row_.device_data(), col_.device_data(),
-             vector.device_data(),
+             vector_x.device_data(),
              &zero,
-             result.device_data()));
+             vector_y.device_data()));
     #endif
           cusparseSetStream(handle, 0);
           break;
@@ -302,5 +356,6 @@ namespace jams {
 }
 
 #undef HAS_CUSPARSE_GENERIC_API
+#undef HAS_MKL_INSPECTOR_EXECUTOR_API
 
 #endif // JAMS_CONTAINERS_SPARSE_MATRIX_H
