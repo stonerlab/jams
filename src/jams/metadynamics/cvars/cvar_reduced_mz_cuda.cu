9ff2fe5679a87927d699e75500f555ca214a7d23
Joseph Barker <j.barker@leeds.ac.uk>
2022-11-08 20:50:31 +0000

    Fix integer division

--- src/jams/metadynamics/cvars/cvar_reduced_mz_cuda.cu
+++ src/jams/metadynamics/cvars/cvar_reduced_mz_cuda.cu
@@ -15,7 +15,7 @@ jams::CVarReducedMzCuda::CVarReducedMzCuda(const libconfig::Setting &settings)
 double jams::CVarReducedMzCuda::value() {
   Vec3 m;
   for (auto i = 0; i < 3; ++i) {
-    m[i] = cuda_stride_reduce_array(globals::s.device_data(), globals::num_spins3, 3, i)  / globals::num_spins;
+    m[i] = cuda_stride_reduce_array(globals::s.device_data(), globals::num_spins3, 3, i);
   }
 
   return m[2] / norm(m);
@@ -26,7 +26,7 @@ const jams::MultiArray<double, 2>&
 jams::CVarReducedMzCuda::derivatives() {
   Vec3 m;
   for (auto i = 0; i < 3; ++i) {
-    m[i] = cuda_stride_reduce_array(globals::s.device_data(), globals::num_spins3, 3, i)  / globals::num_spins;
+    m[i] = cuda_stride_reduce_array(globals::s.device_data(), globals::num_spins3, 3, i) / (globals::num_spins);
   }
 
 //  // WARNING: THIS WILL BE VERY SLOW. WRITE A KERNEL OR FUNCTION LATER
@@ -42,9 +42,9 @@ jams::CVarReducedMzCuda::derivatives() {
 
 //  cuda_reduced_mz_kernel<<<grid_size, block_size>>>(globals::num_spins, m[0], m[1], m[2], derivatives_.device_data());
 
-  const double dx = -(1/globals::num_spins) * m[0]*m[2] / pow3(norm(m));
-  const double dy = -(1/globals::num_spins) * m[1]*m[2] / pow3(norm(m));
-  const double dz = (1/globals::num_spins) * (pow2(norm(m)) - m[2]*m[2]) / pow3(norm(m));
+  const double dx = -(1.0 / globals::num_spins) * m[0]*m[2] / pow3(norm(m));
+  const double dy = -(1.0 / globals::num_spins) * m[1]*m[2] / pow3(norm(m));
+  const double dz = -(1.0 / globals::num_spins) * m[2]*m[2] / pow3(norm(m)) + 1.0 / (globals::num_spins * norm(m));
 
   cuda_reduced_mz_kernel2<<<grid_size, block_size>>>(globals::num_spins, dx, dy, dz, derivatives_.device_data());
 
