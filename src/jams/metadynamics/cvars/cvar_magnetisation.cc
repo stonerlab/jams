83b4cab3fda6f9bc5e823bcdd92a164c1706bd86
Joseph Barker <j.barker@leeds.ac.uk>
2022-12-23 14:14:41 +0000

    Cleanup Will's code
    - Refactored the reading of the settings to keep all of the logic in a single if statement.
    - Renamed `material_` -> `selected_material_id_` and `num_mat_spins_` -> `num_selected_spins_`
    - Fixed a missing division by `num_selected_spins_` in `spin_move_trial_value`.

--- src/jams/metadynamics/cvars/cvar_magnetisation.cc
+++ src/jams/metadynamics/cvars/cvar_magnetisation.cc
@@ -4,23 +4,31 @@
 #include <jams/core/lattice.h>
 
 double jams::CVarMagnetisation::value() {
-  return cached_value() / num_mat_spins_;
+  return cached_value() / num_selected_spins_;
 }
 
 
 double jams::CVarMagnetisation::calculate_expensive_value() {
   double magnetisation = 0.0;
 
+  // No specific material is selected so use all spins
+  if (selected_material_id_ == -1) {
+    for (auto i =0; i < globals::num_spins; ++i) {
+        magnetisation += globals::s(i, magnetisation_component_);
+    }
+    return magnetisation;
+  }
+
+  assert(selected_material_id_ >= 0 && selected_material_id_ < lattice->num_materials());
+
+  // A specific material has been selected in the config so only include
+  // that material in the sum
   for (auto i =0; i < globals::num_spins; ++i) {
-    // if the spin matches the specified material id, or we are considering
-    // all spins in the system, then add that spin to the total magnetisation.
-    if (lattice->atom_material_id(i)==material_ || material_==-1) {
+    if (lattice->atom_material_id(i) == selected_material_id_) {
       magnetisation += globals::s(i, magnetisation_component_);
     }
   }
 
-  // TODO: Check if we should normalise this by the number of spins of relevant material
-  // or not. We do this in spin_move_trial_value()...
   return magnetisation;
 }
 
@@ -28,14 +36,18 @@ double jams::CVarMagnetisation::calculate_expensive_value() {
 double
 jams::CVarMagnetisation::spin_move_trial_value(int i, const Vec3 &spin_initial,
                                                const Vec3 &spin_trial) {
-  if (lattice->atom_material_id(i)==material_ || material_==-1) {
+
+  // Spin 'i' is of chosen material, or all materials are selected
+  if (selected_material_id_ == -1 || lattice->atom_material_id(i) == selected_material_id_) {
     const double trial_value = cached_value() - spin_initial[magnetisation_component_] + spin_trial[magnetisation_component_];
     set_cache_values(i, spin_initial, spin_trial, cached_value(), trial_value);
-    return trial_value / num_mat_spins_;
+    return trial_value / num_selected_spins_;
   }
 
+  // Spin 'i' is not of a selected material so the CV is not changed by this
+  // trial move
   set_cache_values(i, spin_initial, spin_trial, cached_value(), cached_value());
-  return cached_value();
+  return cached_value() / num_selected_spins_;
 }
 
 
@@ -45,30 +57,35 @@ std::string jams::CVarMagnetisation::name() {
 
 
 jams::CVarMagnetisation::CVarMagnetisation(const libconfig::Setting &settings) {
-  std::string def = "all";
-  std::string material = def;
+
+  // The optional setting 'material' can be used to restrict the magnetisation
+  // calculation to a single material. If the setting is omitted then all spins
+  // are used to calculate the magnetisation.
+  //
+  // We stored the selected material by its id in `selected_material_id_`. If
+  // all materials should be used then `selected_material_id_ = -1`.
 
   if (settings.exists("material")) {
-      material = config_optional<std::string>(settings, "material", def);
-      if (material != "all" && !lattice->material_exists(material)) {
-          throw std::runtime_error("Invalid material specified in magnetisation collective variable");
-      }
-  }
-  material_ = (material==def) ? -1 : lattice->material_id(material);
+    std::string material = config_required<std::string>(settings, "material");
+    if (!lattice->material_exists(material)) {
+        throw std::runtime_error("Invalid material specified in magnetisation collective variable");
+    }
+    selected_material_id_ = lattice->material_id(material);
 
-  // Count the number of spins of each type.
-  int count = 0;
-  if (material_==-1) {
-      count = globals::num_spins;
+    // record the number of spins of this material type
+    num_selected_spins_ = 0;
+    for (auto i = 0; i < globals::num_spins; ++i) {
+      if (lattice->atom_material_id(i) == selected_material_id_) {
+        num_selected_spins_++;
+      }
+    }
   } else {
-      for (auto i=0; i < globals::num_spins; ++i) {
-          if (lattice->atom_material_id(i)==material_) {
-              count += 1;
-          }
-      }
+    selected_material_id_ = -1;
+    num_selected_spins_ = globals::num_spins;
   }
 
-  num_mat_spins_ = count;
+  assert((selected_material_id_ >= 0 && selected_material_id_ < lattice->num_materials()) || selected_material_id_ == -1);
+  assert(num_selected_spins_ > 0);
 
   auto component = config_required<std::string>(settings, "component");
 
