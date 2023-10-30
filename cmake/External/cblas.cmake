ce559ef602bf429960aa4fd26b31b6ff9fb223e4
Joseph Barker <j.barker@leeds.ac.uk>
2023-10-30 15:44:34 +0000

    Make cblas.h searching more systematic

--- cmake/External/cblas.cmake
+++ cmake/External/cblas.cmake
@@ -1,87 +1,55 @@
 add_library(cblas_external INTERFACE)
 
-find_package(MKL QUIET)
-if(MKL_FOUND)
-    target_include_directories(cblas_external INTERFACE ${MKL_INCLUDE_DIR})
-    target_link_libraries(cblas_external INTERFACE ${MKL_LIBRARIES})
-    set(JAMS_CBLAS_FOUND true)
-    set(JAMS_CBLAS_VENDOR "mkl")
-    set(BLAS_FOUND ON)
-endif()
+set(BLAS_VENDOR_SEARCH_LIST "MKL" "Apple" "OpenBLAS" "GoTo" "Intel10_64lp_seq" "Intel10_64lp" "Intel10_32" "Intel" "ATLAS" "FLAME" "Generic")
 
-if (NOT BLAS_FOUND)
-    set(BLA_VENDOR Apple)
-    find_package(BLAS QUIET)
-    if(BLAS_FOUND)
+foreach (VENDOR ${BLAS_VENDOR_SEARCH_LIST})
+    if("${VENDOR}" STREQUAL "MKL")
+        find_package(MKL QUIET)
+        if(MKL_FOUND)
+            set(CBLAS_INCLUDE_DIR ${MKL_INCLUDE_DIR})
+            set(BLAS_LIBRARIES ${MKL_LIBRARIES})
+            set(JAMS_CBLAS_FOUND true)
+        endif()
+    else()
+        set(BLA_VENDOR ${VENDOR})
+        find_package(BLAS)
+        if(BLAS_FOUND)
+            set(VENDOR_LOWER $<LOWER_CASE:${VENDOR}>)
+            set(VENDOR_UPPER $<UPPER_CASE:${VENDOR}>)
+            set(CBLAS_INCLUDE_SEARCH_PATHS
+                    $ENV{${VENDOR}_INCLUDE_DIR}
+                    $ENV{${VENDOR_LOWER}_INCLUDE_DIR}
+                    $ENV{${VENDOR_UPPER}_INCLUDE_DIR}
+                    $ENV{${VENDOR}_HOME}
+                    $ENV{${VENDOR_LOWER}_HOME}
+                    $ENV{${VENDOR_UPPER}_HOME}
+                    $ENV{${VENDOR}_ROOT}
+                    $ENV{${VENDOR_LOWER}_ROOT}
+                    $ENV{${VENDOR_UPPER}_ROOT}
+                    $ENV{${VENDOR}_PREFIX}
+                    $ENV{${VENDOR_LOWER}_PREFIX}
+                    $ENV{${VENDOR_UPPER}_PREFIX}
+                    $ENV{BLAS_HOME}
+                    $ENV{BLAS_ROOT}
+                    /usr/local/include
+                    /usr/local/opt
+                    /opt
+            )
 
-        file(TO_CMAKE_PATH "$ENV{Accelerate_HOME}" Accelerate_HOME)
-        SET(Accelerate_INCLUDE_SEARCH_PATHS
-                /Library/Developer/CommandLineTools/SDKs/MacOSX.sdk/System/Library/Frameworks/Accelerate.framework/Versions/A/Frameworks/vecLib.framework/Versions/Current
-                /System/Library/Frameworks/Accelerate.framework/Versions/Current/Frameworks/vecLib.framework/Versions/Current
-                ${Accelerate_HOME}
-        )
+            cmake_path(CONVERT "${CBLAS_INCLUDE_SEARCH_PATHS}" TO_CMAKE_PATH_LIST CBLAS_INCLUDE_SEARCH_PATHS)
+            find_path(CBLAS_INCLUDE_DIR NAMES cblas.h HINTS ${CBLAS_INCLUDE_SEARCH_PATHS} PATH_SUFFIXES "${VENDOR}" "${VENDOR_LOWER}" "include" "include/${VENDOR}" "include/${VENDOR_LOWER}" REQUIRED NO_CACHE)
+            set(JAMS_CBLAS_FOUND true)
+        endif()
+    endif()
 
-        find_path(CBLAS_INCLUDE_DIR NAMES cblas.h PATHS ${Accelerate_INCLUDE_SEARCH_PATHS} PATH_SUFFIXES Headers)
-
+    if(JAMS_CBLAS_FOUND)
         target_include_directories(cblas_external INTERFACE ${CBLAS_INCLUDE_DIR})
         target_link_libraries(cblas_external INTERFACE ${BLAS_LIBRARIES})
-        set(JAMS_CBLAS_FOUND true)
-        set(JAMS_CBLAS_VENDOR "Apple")
+        set(JAMS_CBLAS_VENDOR ${VENDOR})
+        break()
     endif()
-endif()
+endforeach ()
 
-if (NOT BLAS_FOUND)
-    set(BLA_VENDOR OpenBLAS)
-    find_package(BLAS)
-    if(BLAS_FOUND)
-        file(TO_CMAKE_PATH "$ENV{OpenBLAS_HOME}" OpenBLAS_HOME)
-
-        SET(Open_BLAS_INCLUDE_SEARCH_PATHS
-                $ENV{BLAS_ROOT}
-                $ENV{BLAS_ROOT}/include
-                $ENV{BLAS_ROOT}/include/openblas
-                ${OpenBLAS_HOME}
-                ${OpenBLAS_HOME}/include
-                ${OpenBLAS_HOME}/include/openblas
-                /usr/local/include/openblas
-                /usr/local/include/openblas-base
-                /usr/local/opt/openblas/include
-                /opt/OpenBLAS/include
-                /usr/include/openblas
-                /usr/include/openblas-base
-                ${PROJECT_SOURCE_DIR}/3rdparty/OpenBLAS/include
-                ${PROJECT_SOURCE_DIR}/thirdparty/OpenBLAS/include
-                /usr/include
-                /usr/local/include
-        )
-        find_path(CBLAS_INCLUDE_DIR NAMES cblas.h PATHS ${Open_BLAS_INCLUDE_SEARCH_PATHS} PATH_SUFFIXES openblas)
-
-        target_include_directories(cblas_external INTERFACE ${CBLAS_INCLUDE_DIR})
-        target_link_libraries(cblas_external INTERFACE ${BLAS_LIBRARIES})
-        set(JAMS_CBLAS_FOUND true)
-        set(JAMS_CBLAS_VENDOR "OpenBLAS")
-    endif()
-endif()
-
-if (NOT BLAS_FOUND)
-    find_package(BLAS QUIET)
-    if(BLAS_FOUND)
-        file(TO_CMAKE_PATH "$ENV{BLAS_HOME}" BLAS_HOME)
-        SET(BLAS_INCLUDE_SEARCH_PATHS
-                ${BLAS_HOME}
-                ${BLAS_HOME}/include
-                /usr/include
-                /usr/local/include
-        )
-        find_path(CBLAS_INCLUDE_DIR NAMES cblas.h HINTS ${BLAS_INCLUDE_SEARCH_PATHS})
-
-        target_include_directories(cblas_external INTERFACE ${CBLAS_INCLUDE_DIR})
-        target_link_libraries(cblas_external INTERFACE ${BLAS_LIBRARIES})
-        set(JAMS_CBLAS_FOUND true)
-        set(JAMS_CBLAS_VENDOR "generic")
-    endif()
-endif()
-
-if (NOT BLAS_FOUND)
+if (NOT JAMS_CBLAS_FOUND)
     message(FATAL_ERROR "Failed to find a BLAS library")
 endif()
