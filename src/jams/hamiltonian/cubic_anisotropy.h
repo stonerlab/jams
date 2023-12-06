fafe4747b15c5e228f4ab33b22ef7925f793b2a6
Joseph Barker <j.barker@leeds.ac.uk>
2023-12-06 14:48:05 +0000

    Document cubic anisotropy code

--- src/jams/hamiltonian/cubic_anisotropy.h
+++ src/jams/hamiltonian/cubic_anisotropy.h
@@ -7,6 +7,50 @@
 
 #include <jams/core/hamiltonian.h>
 
+///
+/// Hamiltonian for cubic anisotropy
+///
+/// \f[
+///     \mathcal{H} = -\sum_i K1_i [ (S_i . u_i)^2 (S_i . v_i)^2 + (S_i . v_i)^2 (S_i . w_i)^2 + (S_i . u_i)^2 (S_i . w_i)^2 ] -\sum_i K2_i [ (S_i . u_i)^2 (S_i . v_i)^2 (S_i . w_i)^2 ]
+/// \f]
+///
+/// @details K1_i, K2_i are the first and second cubic anisotropy energies
+/// on each site and u_i, v_i, w_i are the local reference axes for each site.
+///
+/// Settings
+/// --------
+/// The Hamiltonian settings are:
+///
+/// module       : "cubic-anisotropy"
+///
+/// energy_units : (string) units of the energies specified in this module
+///
+/// K1            : List of values of anisotropy settings where each setting
+///                 is a list of (energy, [ux, uy, uz], [vx, vy, vz], [wx, wy, wz])
+///
+/// K2            : List of values of anisotropy settings where each setting
+///                 is a list of (energy, [ux, uy, uz], [vx, vy, vz], [wx, wy, wz])
+///
+/// Notes
+/// -----
+/// The uvw axes in the config do not need to be normalised. We normalise them
+/// on reading. This allows the directions to be given in clearer crystallographic
+/// notation.
+///
+/// The uvw axes must be orthogonal. JAMS will raise an error if they are not.
+///
+/// Example
+/// -------
+///
+/// hamiltonians = (
+/// {
+///   module = "cubic";
+///   unit_name = "meV";
+///   K1 = ( ( 0.01, [ 2, -1, -1 ], [ 0, 1, -1 ], [ 1, 1, 1 ] ));
+/// }
+/// );
+///
+
 class CubicHamiltonian : public Hamiltonian {
     friend class CudaCubicHamiltonian;
 
