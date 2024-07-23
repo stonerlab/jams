2340fdf3c3dd21c348db069aae96a6626a15bbac
Joseph Barker <j.barker@leeds.ac.uk>
2024-07-23 13:40:48 +0100

    Add documentation to CrystalFieldHamiltonian

--- src/jams/hamiltonian/crystal_field.h
+++ src/jams/hamiltonian/crystal_field.h
@@ -4,25 +4,57 @@
 #include <jams/core/hamiltonian.h>
 
 ///
-/// The crystal field Hamiltonian will often be used only for rare-earths
-/// within more complex magnets. We read coefficients and calculate the
-/// Hamiltonian only for that subset of spins (to avoid having to write
-/// a lot of zeros in the input file).
+/// Hamiltonian for crystal fields
 ///
-
-
+/// \f[
+///     \mathcal{H} = \sum_{i} \sum_{l=2,4,6}\sum_{m=-l}^l A_{l} B_{l,m} Y_{l,m}(\vec{S}_i)
+/// \f]
+///
+/// Where the A's are defined as
+///
+/// \f[
+///     A_2 &= J (J - 0.5) \alpha_J \\
+///     A_4 &= J (J - 0.5) (J - 1) (J - 1.5) \beta_J
+///     A_6 &= J (J - 0.5) (J - 1) (J - 1.5) (J - 2) (J - 2.5) \gamma_J
+/// \f]
+///
+/// with Stevens factors \alpha_J, \beta_J, \gamma_J and J from Hund's rules. B_{l,m} is
+/// a complex crystal field coefficient and Y_{l,m} is a spherical harmonic. The Hamiltonian
+/// is only implemented for l=2,4,6.
+///
+///
+/// ---------------------------------------------------------------------------
+/// config settings
+/// ---------------------------------------------------------------------------
+///
+/// energy_units: (sting) name of the units of energy of the input.
+///
+/// energy_cutoff: (float, required) absolute energies which are smaller than
+///                this are set to zero. This is a required setting because
+///                it is used to check that the imaginary part of the tesseral
+///                harmonics is zero.
+///
+/// crystal_field_coefficients: (list of lists) each sub list takes the form
+///                (material, J, alphaJ, betaJ, gammaJ, cf_param_filename)
+///                where material can be a name or unit cell positions,
+///                J, alphaJ, betaJ, gammaJ are floats defined in the crystal
+///                field Hamiltonian and cf_param_filename is a filename which
+///                contains the values of B_lm for this material. The file
+///                should contain 6 columns: l m upR upIm dnR dnIm.
+///
 /// Example
 /// -------
 ///
 /// hamiltonians = (
 /// {
 ///   module = "crystal-field";
-///   unit_name = "meV";
-///   # One array per position in the unit cell
-///   # unitcell position or materials, 'B' indices, real and imaginary parts of crystal field coefficient
-///   #  ( atom, [l, m], [Re(B_lm), Im(B_lm)])
-///   crystal_field_coefficients =
-///     (( 1, [0, 0], [0.25, 0.0]),);
+///   debug = false;
+///   energy_units = "meV";
+///   energy_cutoff = 0.001;
+///   crystal_field_coefficients = (
+///     // (material, J, alphaJ, betaJ, gammaJ, cf_param_filename)
+///     ("Tb", 6, -0.01010101, 0.00012244, -0.00000112, "Tb.CFparameters.dat")
+///   );
 /// }
 /// );
 ///
@@ -48,11 +80,11 @@ public:
     using TesseralHarmonicCoefficientMap = std::map<std::pair<int, int>, double>;
 
 protected:
-    // Reads a crystal field coefficient file and returns a map where the key is the pair {l, m} and the value is
-    // the complex crystal field coefficient.
+    // Reads a crystal field coefficient file and returns a SphericalHarmonicCoefficientMap where the key is the pair
+    // {l, m} and the value is the complex crystal field coefficient.
     //
-    // The file to be read should have columns 'l, m, Re(B_lm_up), Im(B_lm_up), Re(B_lm_down), Im(B_lm_down)'. The
-    // 'up' and 'down' data will be averaged.
+    // The file should have columns 'l, m, Re(B_lm_up), Im(B_lm_up), Re(B_lm_down), Im(B_lm_down)', although the
+    // columns Re(B_lm_down) and Im(B_lm_down) are ignored. Any missing values of l,m are set to zero.
     //
     // The returned map will be order (intrinsically due to C++ map) increasing in l and in m, e.g.
     // {0, 0}, {2, -2}, {2, 1}, {2, 0}, {2, 1}, {2, 2}, {4, -4}, {4, -3} ...
@@ -60,10 +92,10 @@ protected:
 
     TesseralHarmonicCoefficientMap convert_spherical_to_tesseral(const SphericalHarmonicCoefficientMap& spherical_coefficients, const double zero_epsilon);
 
-    // Maximum number of crystal field coefficients supported
-    // 27 corresponds to l=2,4,6, m = -l...l.
+    // Maximum number of crystal field coefficients supported. 27 corresponds to l=2,4,6
     unsigned int kCrystalFieldNumCoeff_ = 27;
 
+    // Energy cutoff (in same units as input) to determine zero
     double energy_cutoff_ = 0.0;
 
     // Boolean array of whether a spin has a non-zero crystal field
