486c3515ec46982b4ba31768578fff0cf498836b
Joseph Barker <j.barker@leeds.ac.uk>
2025-05-13 14:31:14 +0100

    Generalise Bloch domain wall initialiser
    We can now specify the direction and orientation of the domain wall. The initialiser also now rotates the spins with respect to the reference domain wall state. This means any existing system (e.g. ferrimagnetic, antiferro, thermalised etc...) is maintained, with the domain wall superimposed.

--- src/jams/initializer/init_bloch_domain_wall.h
+++ src/jams/initializer/init_bloch_domain_wall.h
@@ -15,11 +15,16 @@
 ///
 /// The general equation is
 /// @f[
-///     S_x(x) = 0; S_y(x) = 1/cosh(pi (x-center) / width); S_z = tanh(pi (x-center) / width)
+///     m_x(x) = 0; m_y(x) = 1/cosh(pi (x-center) / width); m_z = tanh(pi (x-center) / width)
 /// @f]
 /// This assumes the system is much wider than the domain wall. See
 /// [Kazantseva, Phys. Rev. Lett. 94, 037206 (2005)](https://dx.doi.org/10.1103/physrevlett.94.037206)
 /// for a derivation and more details of constrained domain walls.
+///
+/// The spin state is set by rotating the spins with respect the mx, my, mz of the domain wall. This means,
+/// for example, if there is a ferrimagnetic ground state already set, the ground state is rotated, rather than
+/// converting the system to a ferromagnetic state. Additional settings `normal` and `domain` allow the
+/// direction and orientation of the domain wall to be chosen.
 ///
 /// @attention We are using the convention for width which includes a factor of
 /// pi. i.e. = pi \sqrt{A/K}. This factor of pi is an arbitrary choice in the
@@ -29,7 +34,10 @@
 ///
 /// @setting `initializer.width` (required) domain wall width in units of lattice constants
 /// @setting `initializer.center` (required) domain wall width in units of lattice constants
-///
+/// @setting `initializer.normal` (optional) propagation direction normal to the domain wall in cartesian space,
+///                               default is x-axis (1, 0, 0)
+/// @setting `initializer.domain` (optional) domain direction in cartesian space, the domain wall will be between
+///                               +domain and -domain, default is z-axis (0, 0, 1)
 /// @example Example config:
 /// @code{.unparsed}
 /// initializer : {
