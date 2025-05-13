2c4fa093aad9fbd1e5ffb5df0c7a1607a5bbb094
Joseph Barker <j.barker@leeds.ac.uk>
2025-05-13 13:28:02 +0100

    Generalise pinned boundaries
    Pinned boundaries can now be defined on any face of the supercell.

--- src/jams/physics/pinned_boundaries.cc
+++ src/jams/physics/pinned_boundaries.cc
@@ -10,81 +10,38 @@
 
 
 PinnedBoundariesPhysics::PinnedBoundariesPhysics(const libconfig::Setting &settings)
-: Physics(settings)
-, left_pinned_magnetisation_(
-    jams::config_required<Vec3>(settings, "left_pinned_magnetisation"))
-, right_pinned_magnetisation_(
-    jams::config_required<Vec3>(settings, "right_pinned_magnetisation")) {
+: Physics(settings) {
+  for (const auto& [name, comp] : boundary_definitions_) {
+    if (!settings.exists(name + "_pinned_magnetisation")) continue;
 
-  // Number of unit cells from 0 along the 'a' direction to pin on the left
-  int left_pinned_cells
-    = jams::config_optional<int>(settings, "left_pinned_cells", 1);
+    PinnedBoundary boundary;
+    boundary.magnetisation = jams::config_required<Vec3>(settings, name + "_pinned_magnetisation");
 
-  // Number of unit cells along the 'a' direction to pin from the right
-  int right_pinned_cells
-    = jams::config_optional<int>(settings, "right_pinned_cells", 1);
+    int num_pinned_cells = jams::config_optional<int>(settings, name + "_pinned_cells", 1);
 
-  // Find which spins are in the left and right pinned regions
-  std::vector<int> left_region_indices;
-  std::vector<int> right_region_indices;
-
-  for (auto i = 0; i < globals::num_spins; ++i) {
-    auto cell = ::globals::lattice->cell_offset(i);
-    if (cell[0] < left_pinned_cells) {
-      left_region_indices.push_back(i);
+    std::vector<int> region_indices;
+    for (auto i = 0; i < globals::num_spins; ++i) {
+      if (comp(num_pinned_cells, globals::lattice->cell_offset(i), globals::lattice->size())) {
+        region_indices.push_back(i);
+      }
     }
-    if (cell[0] >= globals::lattice->size()[0] - right_pinned_cells) {
-      right_region_indices.push_back(i);
-    }
+    boundary.indices = jams::MultiArray<int,1>(region_indices.begin(), region_indices.end());
+
+    boundaries.insert({name, boundary});
   }
-
-  // Store the spin indices in the class
-  left_pinned_indices_ = jams::MultiArray<int,1>(
-      left_region_indices.begin(), left_region_indices.end());
-  right_pinned_indices_ = jams::MultiArray<int,1>(
-      right_region_indices.begin(), right_region_indices.end());
-
-  initialized = true;
 }
 
 void PinnedBoundariesPhysics::update(const int &iterations, const double &time, const double &dt) {
-  // Rotate the spins in the left region so that their magnetisation points
-  // along the pinning direction
-
-  if (globals::solver->is_cuda_solver()) {
-    Vec3 left_mag = jams::vector_field_indexed_scale_and_reduce_cuda(globals::s, globals::mus, left_pinned_indices_);
-
-    auto left_rotation_matrix = rotation_matrix_between_vectors(
-        left_mag, left_pinned_magnetisation_);
-
-    jams::rotate_spins_cuda(globals::s, left_rotation_matrix,
-                            left_pinned_indices_);
-
-    Vec3 right_mag = jams::vector_field_indexed_scale_and_reduce_cuda(globals::s, globals::mus, right_pinned_indices_);
-
-    auto right_rotation_matrix = rotation_matrix_between_vectors(
-        right_mag, right_pinned_magnetisation_);
-
-    jams::rotate_spins_cuda(globals::s, right_rotation_matrix,
-                            right_pinned_indices_);
-
-  } else {
-    Vec3 left_mag = jams::sum_spins_moments(globals::s, globals::mus, left_pinned_indices_);
-
-    auto left_rotation_matrix = rotation_matrix_between_vectors(
-        left_mag, left_pinned_magnetisation_);
-
-    jams::rotate_spins(globals::s, left_rotation_matrix, left_pinned_indices_);
-
-    // Rotate the spins in the right region so that their magnetisation points
-    // along the pinning direction
-
-    Vec3 right_mag = jams::sum_spins_moments(globals::s, globals::mus, right_pinned_indices_);
-
-    auto right_rotation_matrix = rotation_matrix_between_vectors(
-        right_mag, right_pinned_magnetisation_);
-
-    jams::rotate_spins(globals::s, right_rotation_matrix, right_pinned_indices_);
+    for (const auto& [name, boundary] : boundaries) {
+      if (globals::solver->is_cuda_solver()) {
+        Vec3 mag = jams::vector_field_indexed_scale_and_reduce_cuda(globals::s, globals::mus, boundary.indices);
+        auto rotation_matrix = rotation_matrix_between_vectors(mag, boundary.magnetisation);
+        jams::rotate_spins_cuda(globals::s, rotation_matrix, boundary.indices);
+      } else {
+        Vec3 mag = jams::sum_spins_moments(globals::s, globals::mus, boundary.indices);
+        auto rotation_matrix = rotation_matrix_between_vectors(mag, boundary.magnetisation);
+        jams::rotate_spins(globals::s, rotation_matrix, boundary.indices);
+    }
   }
 }
 
