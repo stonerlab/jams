8b4f1296b4c04f58cda82c11593ce0ac27721d06
Joseph Barker <53377004+drjbarker@users.noreply.github.com>
2025-05-13 14:31:47 +0100

    Generalise pinned boundaries (#226)
    * Generalise pinned boundaries
    
    Pinned boundaries can now be defined on any face of the supercell.
    
    * Generalise Bloch domain wall initialiser
    
    We can now specify the direction and orientation of the domain wall. The initialiser also now rotates the spins with respect to the reference domain wall state. This means any existing system (e.g. ferrimagnetic, antiferro, thermalised etc...) is maintained, with the domain wall superimposed.

--- src/jams/initializer/init_bloch_domain_wall.cc
+++ src/jams/initializer/init_bloch_domain_wall.cc
@@ -10,14 +10,24 @@
 void jams::InitBlochDomainWall::execute(const libconfig::Setting &settings) {
   double width = jams::config_required<double>(settings, "width");
   double center = jams::config_required<double>(settings, "center");
+  Vec3 normal = normalize(jams::config_optional<Vec3>(settings, "normal", {1, 0, 0}));
+  Vec3 domain = normalize(jams::config_optional<Vec3>(settings, "domain", {0, 0, 1}));
 
   for (auto i = 0; i < globals::num_spins; ++i) {
     auto r = globals::lattice->lattice_site_position_cart(i);
     // NOTE: The factor of pi here is an arbitrary convention. See the
     // documentation for jams::InitBlochDomainWall.
-    globals::s(i, 0) = 0.0;
-    globals::s(i, 1) = 1.0 / std::cosh(kPi * (r[0]-center) / width);
-    globals::s(i, 2) = std::tanh(kPi * (r[0]-center) / width);
+
+    double x = dot(r, normal) - center;
+    Vec3 m = {0, 1.0 / std::cosh(kPi * x / width), std::tanh(kPi * x / width)};
+
+    Vec3 spin = {globals::s(i, 0), globals::s(i, 1), globals::s(i, 2)};
+    auto rotation_matrix = rotation_matrix_between_vectors(domain, m);
+    spin = rotation_matrix * spin;
+
+    for (auto n = 0; n < 3; ++n) {
+      globals::s(i, n) = spin[n];
+    }
   }
 }
 
--- src/jams/physics/pinned_boundaries.h
+++ src/jams/physics/pinned_boundaries.h
@@ -1,6 +1,9 @@
 #ifndef JAMS_PINNED_BOUNDARIES_PHYSICS_H
 #define JAMS_PINNED_BOUNDARIES_PHYSICS_H
 
+#include <functional>
+#include <unordered_map>
+
 /// @class PinnedBoundaryPhysics
 ///
 /// Maintains the direction of magnetisation in two edge regions by rotating
@@ -13,7 +16,9 @@
 /// in opposite directions.
 ///
 /// @p A 'left' and 'right' region are defined as a number of unit cells starting
-/// from the left and right edges of the system along the 'a' lattice vector.
+/// from the left and right edges of the system along the 'a' lattice vector,
+/// 'front' and 'back' are along the 'b' lattice vector and 'bottom' and 'top'
+/// are along the 'c' lattice vector.
 /// The update() method calculates the magnetisation direction in those regions
 /// and rotates all of the spins so that the magnetisation aligns with preset
 /// pinning directions. The pinning directions can be different on the left
@@ -21,14 +26,30 @@
 /// regions can still have a magnetisation length which is the equilibrium
 /// value.
 ///
-/// @setting `physics.left_pinned_magnetisation` (required) magnetisation pinned
+/// @setting `physics.left_pinned_magnetisation` (optional) magnetisation pinned
 ///           direction on the left side
-/// @setting `physics.right_pinned_magnetisation` (required) magnetisation pinned
+/// @setting `physics.right_pinned_magnetisation` (optional) magnetisation pinned
 ///           direction on the right side
 /// @setting `physics.left_pinned_cells` (optional, default `1`) number of unit
 ///           cells to pin from the left edge
 /// @setting `physics.right_pinned_cells` (optional, default `1`) number of unit
 ///           cells to pin from the right edge
+/// @setting `physics.front_pinned_magnetisation` (optional) magnetisation pinned
+///           direction on the front side
+/// @setting `physics.back_pinned_magnetisation` (optional) magnetisation pinned
+///           direction on the back side
+/// @setting `physics.front_pinned_cells` (optional, default `1`) number of unit
+///           cells to pin from the front edge
+/// @setting `physics.back_pinned_cells` (optional, default `1`) number of unit
+///           cells to pin from the back edge
+/// @setting `physics.bottom_pinned_magnetisation` (optional) magnetisation pinned
+///           direction on the bottom side
+/// @setting `physics.top_pinned_magnetisation` (optional) magnetisation pinned
+///           direction on the top side
+/// @setting `physics.bottom_pinned_cells` (optional, default `1`) number of unit
+///           cells to pin from the bottom edge
+/// @setting `physics.top_pinned_cells` (optional, default `1`) number of unit
+///           cells to pin from the top edge
 ///
 /// @example Example config:
 /// @code{.unparsed}
@@ -48,17 +69,43 @@
 
 class PinnedBoundariesPhysics : public Physics {
 public:
-    PinnedBoundariesPhysics(const libconfig::Setting &settings);
-    ~PinnedBoundariesPhysics() = default;
-    void update(const int &iterations, const double &time, const double &dt);
+    explicit PinnedBoundariesPhysics(const libconfig::Setting &settings);
+    ~PinnedBoundariesPhysics() override = default;
+
+    void update(const int &iterations, const double &time, const double &dt) override;
+
 private:
-    bool initialized;
+    struct PinnedBoundary {
+        Vec3 magnetisation;
+        jams::MultiArray<int, 1> indices;
+    };
 
-    Vec3 left_pinned_magnetisation_ = {0.0, 0.0, -1.0};
-    Vec3 right_pinned_magnetisation_ = {0.0, 0.0, 1.0};
+    // Create the definitions of each boundary. This is based on the dimension ({0,1,2} -> {a, b, c}) and
+    // whether this is the lower boundary (increasing from 0) or the upper boundary (decreasing from
+    // supercell size). The boundary is stored as a comparator which can be called like
+    // `comp(num_pinned_cells, globals::lattice->cell_offset(i), globals::lattice->size())` where true
+    // indicates spin i is included in the boundary boundary.
 
-    jams::MultiArray<int,1> left_pinned_indices_;
-    jams::MultiArray<int,1> right_pinned_indices_;
+    using BoundaryPredicate = std::function<bool(int, const Vec3i&, const Vec3i&)>;
+    static inline std::unordered_map<std::string, BoundaryPredicate> make_boundary_definitions() {
+        auto comp = [](int dim, bool upper) {
+            return [dim, upper](int n, const Vec3i& cell, const Vec3i& size) {
+                return upper ? cell[dim] >= size[dim] - n : cell[dim] < n;
+            };
+        };
+
+        return {
+            {"left",   comp(0, false)},
+            {"right",  comp(0, true)},
+            {"front",  comp(1, false)},
+            {"back",   comp(1, true)},
+            {"bottom", comp(2, false)},
+            {"top",    comp(2, true)},
+        };
+    }
+
+    static inline const auto boundary_definitions_ = make_boundary_definitions();
+    std::unordered_map<std::string, PinnedBoundary> boundaries;
 };
 
 #endif  // JAMS_PINNED_BOUNDARIES_PHYSICS_H
