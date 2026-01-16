8b8ae412c31f225cad6ef51ce1d19500cf068e85
Joseph Barker <j.barker@leeds.ac.uk>
2026-01-16 09:59:15 +0000

    Simplify and generalise the config getters
    This avoids issues in the mixed precision code where Real may also be a double, leading to multiple functions with the same signature.

--- src/jams/interface/config.h
+++ src/jams/interface/config.h
@@ -8,23 +8,111 @@
 #include <libconfig.h++>
 #include <jams/core/types.h>
 #include <jams/helpers/utils.h>
+#include <array>
+#include <type_traits>
+
 
 void overwrite_config_settings(libconfig::Setting& orig, const libconfig::Setting& patch);
 
 namespace jams {
 
-    /// Returns the value of the setting `name` within the group of settings
-    /// `s`. If the setting is not found a runtime_error exception is thrown.
-    ///
-    /// Usually the template parameter type `T` should be specified to ensure
-    /// the setting is converted to the correct type in ambigious cases (e.g.
-    /// int vs float).
-    ///
-    /// @example
-    /// auto my_param = config_required<double>(some_settings, "value");
-    ///
-    template<typename T>
-    inline T config_required(const libconfig::Setting &s, const std::string &name);
+namespace detail {
+  template<typename>
+  struct dependent_false : std::false_type {};
+}
+
+template<typename T, typename Enable = void>
+struct config_required_impl {
+  static T get(const libconfig::Setting &, const std::string &) {
+    static_assert(detail::dependent_false<T>::value,
+                  "jams::config_required: unsupported type");
+  }
+};
+
+// Generic implementation for arithmetic types (int/float/double/bool/etc.)
+template<typename T>
+struct config_required_impl<T, std::enable_if_t<std::is_arithmetic_v<T>>> {
+  static T get(const libconfig::Setting &setting, const std::string &name) {
+    return static_cast<T>(setting[name]);
+  }
+};
+
+template<typename T>
+inline T config_required(const libconfig::Setting &s, const std::string &name) {
+  return config_required_impl<T>::get(s, name);
+}
+
+// Specialisation for std::array<T, N>
+template<typename T, std::size_t N>
+struct config_required_impl<std::array<T, N>, void> {
+  static std::array<T, N> get(const libconfig::Setting &s, const std::string &name) {
+    if (s.getLength() != N)
+    {
+      throw std::runtime_error("config_required: array size mismatch");
+    }
+    std::array<T, N> out{};
+    const auto &arr = s[name];
+    for (std::size_t i = 0; i < N; ++i) {
+      out[i] = static_cast<T>(arr[static_cast<int>(i)]);
+    }
+    return out;
+  }
+};
+
+// Specialisations for scalar / simple types
+template<>
+struct config_required_impl<std::string, void> {
+  static std::string get(const libconfig::Setting &setting, const std::string &name) {
+    return setting[name].c_str();
+  }
+};
+
+  // Specialisation for nested arrays: std::array<std::array<T, N>, M>
+  template<typename T, std::size_t N, std::size_t M>
+  struct config_required_impl<std::array<std::array<T, N>, M>, void> {
+    static std::array<std::array<T, N>, M> get(const libconfig::Setting &s, const std::string &name)
+    {
+      std::array<std::array<T, N>, M> out{};
+      const auto &mat = s[name];
+
+      for (std::size_t i = 0; i < M; ++i) {
+        const auto &row = mat[static_cast<int>(i)];
+        for (std::size_t j = 0; j < N; ++j) {
+          out[i][j] = static_cast<T>(row[static_cast<int>(j)]);
+        }
+      }
+
+      return out;
+    }
+  };
+
+template<>
+struct config_required_impl<CoordinateFormat, void> {
+  static CoordinateFormat get(const libconfig::Setting &setting, const std::string &name) {
+    auto format = jams::config_required<std::string>(setting, name);
+    if (lowercase(format) == "fractional") {
+      return CoordinateFormat::FRACTIONAL;
+    } else if (lowercase(format) == "cartesian") {
+      return CoordinateFormat::CARTESIAN;
+    } else {
+      throw std::runtime_error("Unknown coordinate format");
+    }
+  }
+};
+
+template<>
+struct config_required_impl<InteractionFileFormat, void> {
+  static InteractionFileFormat get(const libconfig::Setting &setting, const std::string &name) {
+    auto format = jams::config_required<std::string>(setting, name);
+    if (lowercase(format) == "jams") {
+      return InteractionFileFormat::JAMS;
+    } else if (lowercase(format) == "kkr") {
+      return InteractionFileFormat::KKR;
+    } else {
+      throw std::runtime_error("Unknown interaction file format");
+    }
+  }
+};
 
     /// Returns the value of the setting `name` within the group of settings
     /// `s`. If the setting is not found the default value `def` is returned.
@@ -45,101 +133,6 @@ namespace jams {
         }
     }
 
-    template<>
-    inline std::string config_required(const libconfig::Setting &setting, const std::string &name) {
-      return setting[name].c_str();
-    }
-
-    template<>
-    inline bool config_required(const libconfig::Setting &setting, const std::string &name) {
-      return bool(setting[name]);
-    }
-
-    template<>
-    inline int config_required(const libconfig::Setting &setting, const std::string &name) {
-      return int(setting[name]);
-    }
-
-    template<>
-    inline long config_required(const libconfig::Setting &setting, const std::string &name) {
-      return long(setting[name]);
-    }
-
-    template<>
-    inline unsigned config_required(const libconfig::Setting &setting, const std::string &name) {
-      return unsigned(setting[name]);
-    }
-
-    template<>
-    inline unsigned long config_required(const libconfig::Setting &setting, const std::string &name) {
-      return (unsigned long)(setting[name]);
-    }
-
-    template<>
-    inline double config_required(const libconfig::Setting &setting, const std::string &name) {
-      return double(setting[name]);
-    }
-
-    template<>
-    inline float config_required(const libconfig::Setting &setting, const std::string &name) {
-      return float(setting[name]);
-    }
-
-    template<>
-    inline Vec2 config_required(const libconfig::Setting &setting, const std::string &name) {
-      return {double(setting[name][0]), double(setting[name][1])};
-    }
-
-    template<>
-    inline Vec3 config_required(const libconfig::Setting &setting, const std::string &name) {
-      return {double(setting[name][0]), double(setting[name][1]), double(setting[name][2])};
-    }
-
-    template<>
-    inline Vec3R config_required(const libconfig::Setting &setting, const std::string &name) {
-        return {jams::Real(setting[name][0]), jams::Real(setting[name][1]), jams::Real(setting[name][2])};
-    }
-
-    template<>
-    inline Vec3b config_required(const libconfig::Setting &setting, const std::string &name) {
-      return {bool(setting[name][0]), bool(setting[name][1]), bool(setting[name][2])};
-    }
-
-    template<>
-    inline Vec3i config_required(const libconfig::Setting &setting, const std::string &name) {
-      return {int(setting[name][0]), int(setting[name][1]), int(setting[name][2])};
-    }
-
-    template<>
-    inline Mat3 config_required(const libconfig::Setting &setting, const std::string &name) {
-      return {setting[name][0][0], setting[name][0][1], setting[name][0][2],
-              setting[name][1][0], setting[name][1][1], setting[name][1][2],
-              setting[name][2][0], setting[name][2][1], setting[name][2][2]};
-      }
-
-    template<>
-    inline CoordinateFormat config_required(const libconfig::Setting &setting, const std::string &name) {
-      auto format = jams::config_required<std::string>(setting, name);
-      if (lowercase(format) == "fractional") {
-        return CoordinateFormat::FRACTIONAL;
-      } else if (lowercase(format) == "cartesian") {
-        return CoordinateFormat::CARTESIAN;
-      } else {
-        throw std::runtime_error("Unknown coordinate format");
-      }
-    }
-
-    template<>
-    inline InteractionFileFormat config_required(const libconfig::Setting &setting, const std::string &name) {
-      auto format = jams::config_required<std::string>(setting, name);
-      if (lowercase(format) == "jams") {
-        return InteractionFileFormat::JAMS;
-      } else if (lowercase(format) == "kkr") {
-        return InteractionFileFormat::KKR;
-      } else {
-        throw std::runtime_error("Unknown interaction file format");
-      }
-    }
 
 }
 
