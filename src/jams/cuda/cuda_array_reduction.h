22daa552028446d56931e3596931a12fc086697d
Joseph Barker <j.barker@leeds.ac.uk>
2026-02-11 09:47:09 +0000

    cuda: fix CPU-only template stub warning

--- src/jams/cuda/cuda_array_reduction.h
+++ src/jams/cuda/cuda_array_reduction.h
@@ -22,12 +22,20 @@ namespace jams {
             Vec3 vector_field_scale_and_reduce_cuda(const jams::MultiArray<double, 2> &x,
                                                     const jams::MultiArray<double, 1> &scale_factors));
 
-
+    #if HAS_CUDA
     CUDA_ONLY_IMPLEMENTATION(
             template<typename X, typename S>
             std::array<X,3> vector_field_indexed_scale_and_reduce_cuda(const jams::MultiArray<X, 2> &x,
                                                             const jams::MultiArray<S, 1> &scale_factors,
                                                             const jams::MultiArray<int, 1> &indices));
+    #else
+    template<typename X, typename S>
+    inline std::array<X,3> vector_field_indexed_scale_and_reduce_cuda(const jams::MultiArray<X, 2> &x,
+                                                            const jams::MultiArray<S, 1> &scale_factors,
+                                                            const jams::MultiArray<int, 1> &indices) {
+        throw std::runtime_error("vector_field_indexed_scale_and_reduce_cuda not implemented for CPU only build");
+    }
+    #endif
 #if HAS_CUDA
         CUDA_ONLY_IMPLEMENTATION(
                 float scalar_field_reduce_cuda(const jams::MultiArray<float, 1> &x, cudaStream_t stream = nullptr));
