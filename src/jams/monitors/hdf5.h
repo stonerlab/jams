159d4db9c23ab43e52c3e03a485d5c87cd0ac103
Joseph Barker <53377004+drjbarker@users.noreply.github.com>
2026-02-16 12:01:32 +0000

    Fix Hdf5Monitor teardown lifetime issues (#252)

--- src/jams/monitors/hdf5.h
+++ src/jams/monitors/hdf5.h
@@ -15,26 +15,27 @@ class Solver;
 class Hdf5Monitor : public Monitor {
  public:
   explicit Hdf5Monitor(const libconfig::Setting &settings);
-  ~Hdf5Monitor();
+  ~Hdf5Monitor() override;
 
   void update(Solver& solver) override;
-    void post_process() override {};
+  void post_process() override;
 
  private:
-    void write_vector_field(const jams::MultiArray<double, 2>& field, const std::string& data_path, HighFive::File &file) const;
-    void write_scalar_field(const jams::MultiArray<double, 1>& field, const std::string& data_path, HighFive::File &file) const;
-    void open_new_xdmf_file(const std::string &xdmf_file_name);
-    void update_xdmf_file(const std::string &h5_file_name, const double time);
-    void write_lattice_h5_file(const std::string &h5_file_name);
-    void write_spin_h5_file(const std::string &h5_file_name);
+  void write_vector_field(const jams::MultiArray<double, 2>& field, const std::string& data_path, HighFive::File &file) const;
+  void write_scalar_field(const jams::MultiArray<double, 1>& field, const std::string& data_path, HighFive::File &file) const;
+  void open_new_xdmf_file(const std::string &xdmf_file_name);
+  void update_xdmf_file(const std::string &h5_file_name, const double time);
+  void write_lattice_h5_file(const std::string &h5_file_name);
+  void write_spin_h5_file(const std::string &h5_file_name);
+  void write_final_output();
 
   bool         write_ds_dt_ = false;
   bool         write_fields_ = false;
   bool         write_energies_ = false;
   bool         compression_enabled_ = false;
+  bool         final_output_written_ = false;
   Slice        slice_;
-  FILE*        xdmf_file_;
+  FILE*        xdmf_file_ = nullptr;
 };
 
 #endif  // JAMS_MONITOR_HDF5_H
-
